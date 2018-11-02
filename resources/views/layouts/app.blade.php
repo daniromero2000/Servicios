746aa6e9ad45eda2618cc52be31e7dde23757803
Robert Garcia <robert930810@gmail.com>
2018-11-02 15:34:02 -0500

    Quitando public V3

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -40,8 +40,8 @@
 		<script type="text/javascript" src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
 		<link href="{{ asset('editor/contentbuilder/contentbuilder.css')}}" rel="stylesheet" type="text/css" /> 
 		<link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
-		<script type="text/javascript" src="{{asset('public/js/validateV2.js')}}"></script>
-		<script type="text/javascript" src="{{asset('public/js/script.js')}}"></script>
+		<script type="text/javascript" src="{{asset('js/validateV2.js')}}"></script>
+		<script type="text/javascript" src="{{asset('js/script.js')}}"></script>
 	</head>
 	<body>
 		<div id="preHeader">
