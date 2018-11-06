8ad48f5d30bada67be99cb2d509779411f438e65
Sebastian <desarrollo@lagobo.com>
2018-11-06 16:49:17 -0500

    Ajustes Footer

--- storage/framework/views/d6adc54207f1657109077a9a4b5566e89f90e533.php
+++ storage/framework/views/d6adc54207f1657109077a9a4b5566e89f90e533.php
@@ -138,13 +138,6 @@
 					<div class="footer-contianerLogo">
 						<img src="<?php echo e(asset('images/footer-oportunidadesServiciosFinancierosLogo.png')); ?>" title="Oportunidades Servicios Financieros" class="img-fluid">
 					</div>
-					<div class="footer-contianerTelefonos">
-						<img src="<?php echo e(asset('images/footer-telefonoIcon.png')); ?>" alt="Línea Nacional" class="img-fluid footer-imgNosotros" />
-						<p class="footer-textTelefonos">
-							<span class="footer-textTelefonosNal">Línea nacional: 57 (1)484 2122</span> <br />
-							<span class="footer-textHorario">Lunes a Viernes 8:00 am a 5:00 pm</span>
-						</p>
-					</div>
 					<div class="footer-contianerNosotros">
 						<ul class="footer-menuNosotros">
 							<h5 class="footer-menuTitle">NOSOTROS</h5>
@@ -158,6 +151,13 @@
 				<div class="col-12 col-md-12 col-lg-6 resetCol footer-containMenu">
 					<h4 class="text-center footer-title">Si tienes alguna inquietud <strong>¡Contáctanos!</strong></h4>
 					<div class="footer-containerServicioCliente">
+						<div class="footer-contianerTelefonos">
+							<img src="<?php echo e(asset('images/footer-telefonoIcon.png')); ?>" alt="Línea Nacional" class="img-fluid footer-imgNosotros" />
+							<p class="footer-textTelefonos">
+								<span class="footer-textTelefonosNal">Línea nacional: 57 (1)484 2122</span> <br />
+								<span class="footer-textHorario">Lunes a Viernes 8:00 am a 5:00 pm</span>
+							</p>
+						</div>
 						<ul class="footer-menu">
 							<h5 class="footer-menuTitle" >SERVICIO AL CLIENTE</h5>
 							<li><a href="#" class="footer-menuItem" title="Por qué comprar con nosotros">Por qué comprar con nosotros</a></li>
