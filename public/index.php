ac8cd556df40fc3a40d72758b13d777d01a5586b
Sebastian <desarrollo@lagobo.com>
2018-11-06 15:10:25 -0500

    Arreglo index.php

--- public/index.php
+++ public/index.php
@@ -21,7 +21,7 @@ define('LARAVEL_START', microtime(true));
 |
 */
 
-require __DIR__.'/../vendor/autoload.php';
+require _DIR_.'/../serviciosoportunidades.com/vendor/autoload.php';
 
 /*
 |--------------------------------------------------------------------------
@@ -35,7 +35,7 @@ require __DIR__.'/../vendor/autoload.php';
 |
 */
 
-$app = require_once __DIR__.'/../bootstrap/app.php';
+$app = require_once __DIR__.'/../serviciosoportunidades.com/bootstrap/app.php';
 
 $app->bind('path.public', function() {
     return __DIR__;
