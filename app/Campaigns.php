6529741676244083a698ba11dbb4d391f5324895
Robert Garcia <robert930810@gmail.com>
2018-11-22 14:26:04 -0500

    add campaign controller

---
+++ app/Campaigns.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Campaigns extends Model
+{
+    protected $table = 'campaigns';
+
+    protected $fillable = ['name', 'description', 'socialNetwork', 'beginDate', 'endingDate', 'budget', 'usedBudget'];
+}
