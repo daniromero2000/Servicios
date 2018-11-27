85cc80f3ed7097a1e6ad3e5e22c74000fa5c60e0
Luis David Giraldo Grajales <luis.giraldo@laso.local>
2018-11-27 17:59:06 -0500

    add faqs routes

--- routes/web.php
+++ routes/web.php
@@ -97,6 +97,7 @@ Route::resource('Nuestras-tiendas','Admin\ourStoresController');
 Route::resource('communityleads','Admin\CommunityController');
 Route::resource('oportuyaV2','Admin\OportuyaV2Controller');
 Route::resource('fqas','Admin\fqasController');
+Route::get('preguntas-frecuentes','Admin\fqasController@indexPublic');
 
 Route::get('api/encryptText/{string}','Admin\OportuyaV2Controller@encrypt');
 Route::get('/step3/{numIdentification}', 'Admin\OportuyaV2Controller@step3')->name('step3Oportuya');
