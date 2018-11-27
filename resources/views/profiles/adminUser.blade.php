fa41b94f24d662facd8d2955ead815115cc9f7e5
Luis David Giraldo Grajales <luis.giraldo@laso.local>
2018-11-27 18:14:59 -0500

    add a card for FAQ's

--- resources/views/profiles/adminUser.blade.php
+++ resources/views/profiles/adminUser.blade.php
@@ -1,21 +1,35 @@
+<style>
+	.card{
+		padding: 10px;
+	}
+</style>
+	<div class="card-group">
+	  <div class="card">
+	  	<div class="card-header">
+		    <h5 class="card-title" style="text-align: center;">Gestión de Usuarios</h5>
+		</div>
+	    <div class="card-body">
+			<a href="{{route('users.index')}}"><span><i class="fas fa-users" style="font-size: 70px;"></i></span></a>
+	    </div>
+	  </div>
+	  <div class="card">
+	  	<div class="card-header">
+		    <h5 class="card-title" style="text-align: center;">Gestión de Leads</h5>
+		</div>
+	    <div class="card-body">
+			<a href="/canalDigital"><span><i class="fas fa-file-medical-alt" style="font-size: 70px;"></i></span></a>
+	    </div>
+	  </div>
+	  <div class="card">
+	  	<div class="card-header">
+		    <h5 class="card-title" style="text-align: center;">Gestión de FAQs</h5>
+		</div>
+	    <div class="card-body">
+			<a href="{{route('fqas.index')}}"><span><i class="fas fa-question" style="font-size: 70px;"></i></span></a>
+	    </div>
+	  </div>
 
-	
-	<div class="row userInformation">
-			
-			<div class="col-6">
-				<div class="iconModule">
-					<a href="{{route('users.index')}}"><span><i class="fas fa-users"></i></span></a>
-					<span>Gestión de Usuarios</span>
-				</div>
-			</div>
-
-			<div class="col-6">
-				<div class="iconModule">
-					<a href="/canalDigital"><span><i class="fas fa-file-medical-alt"></i></span>
-					<span>Gestión de Leads</span>
-					</a>
-				</div>
-			</div>
-			
+	 
 	</div>
+
 
