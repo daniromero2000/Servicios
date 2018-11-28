907c62e0826e4ceb4012d1fcd62d7d4b93dbd336
Robert Garcia <robert930810@gmail.com>
2018-11-28 11:04:33 -0500

    disable timestamps at OportuyaV2 model

--- app/OportuyaV2.php
+++ app/OportuyaV2.php
@@ -6,7 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class OportuyaV2 extends Model
 {
-        protected $table = 'CLIENTE_FAB';
+    protected $table = 'CLIENTE_FAB';
+
+    public $timestamps = false;
 
     protected $fillable = ['NOMBRES', 'APELLIDOS', 'EMAIL', 'CELULAR', 'termsAndConditions', 'TIPO_DOC', 'CEDULA','PROFESION','TIPOCLIENTE','SUBTIPO','SEXO','FEC_EXP','CIUD_EXP','CIUD_UBI','DEPTO','TIPOV','TIEMPO_VIV','PROPIETARIO','DIRECCION','VRARRIENDO','TELFIJO','ESTRATO','FEC_NAC','EDAD','ESTADOCIVIL','NOMBRE_CONYU','CEDULA_C','CELULAR_CONYU','TRABAJO_CONYU','PROFESION_CONYU','CARGO_CONYU','SALARIO_CONYU','EPS_CONYU','ESTUDIOS','POSEEVEH','PLACA'];
 }
