0d7fba2b53f625ee72d95c2d2744d43783dccc85
Robert Garcia <robert930810@gmail.com>
2018-11-28 09:03:52 -0500

    fix oportuyaV2

--- app/OportuyaV2.php
+++ app/OportuyaV2.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class OportuyaV2 extends Model
 {
-        protected $table = 'V2';
+        protected $table = 'CLIENTE_FAB';
 
     protected $fillable = ['NOMBRES', 'APELLIDOS', 'EMAIL', 'CELULAR', 'termsAndConditions', 'TIPO_DOC', 'CEDULA','PROFESION','TIPOCLIENTE','SUBTIPO','SEXO','FEC_EXP','CIUD_EXP','CIUD_UBI','DEPTO','TIPOV','TIEMPO_VIV','PROPIETARIO','DIRECCION','VRARRIENDO','TELFIJO','ESTRATO','FEC_NAC','EDAD','ESTADOCIVIL','NOMBRE_CONYU','CEDULA_C','CELULAR_CONYU','TRABAJO_CONYU','PROFESION_CONYU','CARGO_CONYU','SALARIO_CONYU','EPS_CONYU','ESTUDIOS','POSEEVEH','PLACA'];
 }
