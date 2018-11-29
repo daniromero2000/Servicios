706dcc909742d75878b03bcb15abbb938b1e386c
Robert Garcia <robert930810@gmail.com>
2018-11-29 10:53:11 -0500

    debbuger controller

--- app/Http/Controllers/Admin/OportuyaV2Controller.php
+++ app/Http/Controllers/Admin/OportuyaV2Controller.php
@@ -164,7 +164,7 @@ class OportuyaV2Controller extends Controller
 
 			//$oportudataLead=OportuyaV2::findOrFail($identificationNumber);
 
-			$idLead=Lead::select('id')->where('identificationNumber','=',$identificationNumber); 
+			$idLead=DB::select('id')->where('identificationNumber','=',$identificationNumber)->get(); 
 			return $idLead;
 			//$idLead=$idLead[0]->id;
 			
