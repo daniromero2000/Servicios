bffe539cbd376fdb6773b412f9867ba0b1343776
Robert Garcia <robert930810@gmail.com>
2018-12-18 10:24:44 -0500

    button assessors styles

--- resources/views/assessors/dashboard.blade.php
+++ resources/views/assessors/dashboard.blade.php
@@ -1,10 +1,22 @@
 @extends('layouts.app')
 @section('content')
 	<div class="container">
+		<div class="row text-center assessorHeader">
+			<h3>
+				 {{Auth::user()->NOMBRE}}
+			</h3>
+		</div>
 		<div class="row">
-			<p>
-				<a href="{{route('step1Assessor')}}">Crédito Oportuya </a>
-			</p>
+			<div class="col-6 assessorModule text-center">
+				<p>
+					<a href="{{route('step1Assessor')}}">Crédito Oportuya </a>
+				</p>	
+			</div>
+			<div class="col-6 assessorModule text-center">
+				<p>
+					<a href="{{route('step1Assessor')}}">Clientes </a>
+				</p>	
+			</div>			
 		</div>
 	</div>
 @endsection
