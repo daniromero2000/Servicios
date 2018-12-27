cbe94c4fb67d21cdb8fa2743ad7a85b96602503f
Juan Sebastian Ormaza Quirama <juan.ormaza@laso.local>
2018-12-27 07:48:45 -0500

    Probando el web service V2

--- app/Http/Controllers/Admin/OportuyaV2Controller.php
+++ app/Http/Controllers/Admin/OportuyaV2Controller.php
@@ -52,12 +52,11 @@ class OportuyaV2Controller extends Controller
 			$identificationNumber = $request->get('identificationNumber');
 			$dateConsultaComercial = $this->validateDateConsultaComercial($identificationNumber);
 			if($dateConsultaComercial == 'true'){
-				// "Se realiza la consulta";
+				//$consultaComercial = $this->execConsultaComercial($identificationNumber, $request->get('typeDocument'));
 			}else{
 				// "No se realiza la consulta";
 			}
-			$consultaComercial = $this->execConsultaComercial($identificationNumber, $request->get('typeDocument'));
-			return $consultaComercial;
+			
 			//catch data from request and values assigning to leads table columns
 			$departament = $this->getCodeAndDepartmentCity($request->get('city'));
 			$flag=0;
