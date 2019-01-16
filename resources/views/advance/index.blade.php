d03ddf1a40bf7021c2fd93c9cc86dbf90df6b685
Robert Sneyder García Moreno <robert.garcia@laso.local>
2019-01-16 15:39:17 -0500

    add advance elements

---
+++ resources/views/advance/index.blade.php
@@ -0,0 +1,70 @@
+@extends('layouts.app')
+
+@section('title', 'Crédito para Motos')
+
+@section('metaTags')
+	{{-- <meta name="description" content="">
+	<meta name="keywords" content="">
+	<meta property="og:title" content="" />
+	<meta property="og:url" content="" />
+	<meta property="og:type" content="" />
+	<meta property="og:image" content="" />
+	<meta property="og:description" content=""> --}}
+@endsection()
+@section('content')
+	<div id="construccion">
+		<div class="container">
+			<h2 class="creditoLibranza-title text-center">Esta sección está actualmente en construcción</h2>
+			<p class="text-center">Si te interesa conocer más sobre nuestros créditos para motos, déjanos tus datos y un asesor se pondrá en contacto</p>
+			<div class="modalFormulario-body" style="margin: auto;">
+				<div class="modal-containerFormulario">
+					<h3 class="modal-titleForm titleForm-motos">Avance</h3>
+					<form role=form method="POST" id="saveLeadadvance" action="{{ route('avance.store') }}">
+						{{ csrf_field() }}
+						<input type="hidden" name="typeProduct" value="avance">
+						<input type="hidden" name="typeService" value="avance">
+						<input type="hidden" name="channel" value="1">
+						<div class="form-group">
+							<label for="name" class="control-label">Nombres</label>
+							<input type="text" name="name" id="name" class="form-control" validation-pattern="name" required="true"  />
+						</div>
+						<div class="form-group">
+							<label for="lastName" class="control-label">Apellidos</label>
+							<input type="text" name="lastName" id="lastName" class="form-control" validation-pattern="name" required="true"/>
+						</div>
+						<div class="form-group">
+							<label for="email" class="control-label">Correo electrónico</label>
+							<input type="email" name="email" id="mail" class="form-control" validation-pattern="email" required="true"/>
+						</div>
+						<div class="form-group">
+							<label for="telephone class="control-label">Teléfono</label>
+							<input type="text" name="telephone" id="telephone" class="form-control" validation-pattern="telephone" required="true"/>
+						</div>
+						<div class="form-group">
+							<label for="ciudad class="control-label">Ciudad</label>
+							<select name="city" id="city" class="form-control" >
+								@foreach($cities as $city)
+									<option value="{{ $city['value'] }}">{{ $city['label'] }}</option>
+								@endforeach
+							</select>
+						</div>
+						<div class="form-group">
+							<input type="checkbox" name="termsAndConditions" id="termsAndConditions" value="1" required>
+							<label for="termsAndConditions" style="font-size: 13px; font-style: italic;">
+								Aceptar <a href="/Terminos-y-condiciones" class="linkTermAndCondition" target="_blank">términos y condiciones</a> y <a href="/Proteccion-de-datos-personales" class="linkTermAndCondition" target="_blank">política de tratamiento de datos</a>
+							</label>
+						</div>
+						<p class="textCityForm">
+							*Válido solo para ciudades que se desplieguen en la casilla.
+						</p>
+						<div class="form-group text-right">
+							<button type="submit" class="btn btn-primary buttonFormModal buttonFormModalSubmit">
+								Guardar
+							</button>
+						</div>
+					</form>
+				</div>
+			</div>
+		</div>
+	</div>
+@endsection
