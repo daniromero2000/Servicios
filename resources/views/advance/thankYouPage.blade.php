9545409252c00ea5c1bce344d70f66501ad870d7
Robert Sneyder García Moreno <robert.garcia@laso.local>
2019-01-16 16:12:26 -0500

    updating advance form

---
+++ resources/views/advance/thankYouPage.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.app')
+
+@section('content')
+	<div class="row resetRow">
+		<div class="col-12 text-center resetCol thankContainer">
+			<div class="containerThankPage">	
+				<img src="{{ asset('images/imageThankPage.jpg')}}" class="img-fluid">
+			</div>
+			<div class="dialogThakPage">	
+				<img src="{{ asset('images/dialogThankPage.png')}}" class="img-fluid">
+				<p>	Gracias por contactarte con nosotros, un asesor de Avances se comunicará contigo para darte más información.</p>
+			</div>
+		</div>
+	</div>
+@endsection()
