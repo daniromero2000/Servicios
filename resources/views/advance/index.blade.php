c0be8ebe6dc63b1c7e3ff3c0877baede8fabc15d
Robert Sneyder García Moreno <robert.garcia@laso.local>
2019-02-15 08:06:36 -0500

    fix bugs

--- resources/views/advance/index.blade.php
+++ resources/views/advance/index.blade.php
@@ -103,7 +103,7 @@
 						</div>
 						<p class="descriptionRequirements descriptionRequirementsAdvance">
 						<br>	
-						Solo debes ingresar los datos y te llamaremos en menos de 2 horas, o si quieres ir a nuestras oficinas ubicadas en 48 ciudades del Pais, Cualquiera de nuestros asesores estará listo para atenderte. <br> <br>	<b>Te esperamos! </b>
+						Solo debes ingresar los datos y te llamaremos, o si quieres ir a nuestras oficinas ubicadas en 48 ciudades del Pais, Cualquiera de nuestros asesores estará listo para atenderte. <br> <br>	<b>Te esperamos! </b>
 						</p>
 						<div class="row" style="padding-bottom: 15px;">
 							<div class="col-12 text-center">
