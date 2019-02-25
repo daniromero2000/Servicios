14432914b26e235008aea2e6d84616ef79756d6a
Juan Sebastian Ormaza Quirama <juan.ormaza@laso.local>
2019-02-25 11:16:37 -0500

    Analisis modelo

---
+++ app/Analisis.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Analisis extends Model
+{
+    public $table='ANALISIS';
+
+    public $connection='oportudata';
+
+    protected $primaryKey= 'solicitud';
+
+    protected $fillable=['solicitud', 'ini_analis', 'ini_ref','fec_datacli', 'fec_datacod1', 'fec_datacod2', 'valor', 'rf_fpago', 'fin_analis', 'fin_analis', 'Fin_ref', 'autoriz', 'fact_aur', 'ini_def', 'fin_def', 'fec_aur', 'aurfe_cli1', 'aurfe_cli3', 'aurfe_cli3', 'aurfe_cod1', 'aurfe_cod12', 'aurfe_cod13', 'aurfe_cod2', 'aurfe_cod21', 'aurfe_cod22', 'aurcu_cli1', 'aurcu_cli2', 'aurcu_cli3', 'aurcu_cod1', 'aurcu_cod12', 'aurcu_cod13', 'aurcu_cod2'];
+    
+    public $timestamps = false;
+}
