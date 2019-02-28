7a070a6ff76a7aed7fc2ab2b20ab310fd1717dd9
Robert Sneyder García Moreno <robert.garcia@laso.local>
2019-02-28 17:26:33 -0500

    update libranza routes

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,22 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::resource('libranzaV2','Admin\LibranzaV2Controller');
+
+/*Route::group(['prefix'=>'/creditoLibranza/'],function(){
+
+    Route::get('/step1','Admin\LibranzaV2Controller@step1')->name('step1Libranza');
+    Route::get('/getDataStep1/{numIdentification}','Admin\LibranzaV2Controller@getDataStep1');
+    Route::get('/getDataStep2/{numIdentification}','Admin\LibranzaV2Controller@getDataStep2');
+    Route::get('/getDataStep3/{numIdentification}','Admin\LibranzaV2Controller@getDataStep3');
+    Route::post('/saveStep1','Admin\LibranzaV2Controller@store')->name('libranza.saveStep1');
+    Route::post('/saveStep2','Admin\LibranzaV2Controller@store')->name('libranza.saveStep1');
+    Route::get('/step2/{numIdentification}', 'Admin\LibranzaV2Controller@step2')->name('libranzaStep2');
+    Route::get('/decryptText/{string}','Admin\LibranzaV2Controller@decrypt');
+    Route::get('/step2Data/{numIdentification}','Admin\LibranzaV2Controller@getIDStep2');
+    Route::get('/step3/{numIdentification}', 'Admin\LibranzaV2Controller@step3')->name('libranzaStep3');
+    Route::get('/encryptText/{string}','Admin\LibranzaV2Controller@encrypt');
+    Route::get('/cities', 'Admin\LibranzaV2Controller@cities')->name('step1Cities');
+
+})  ;*/
