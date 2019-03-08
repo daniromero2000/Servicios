16030f617dde7bb8d26e5a3c50354ef6226437d2
Robert Sneyder García Moreno <robert.garcia@laso.local>
2019-03-08 12:01:39 -0500

    updating libranza page

--- public/js/libranza.js
+++ public/js/libranza.js
@@ -1,15 +1,5 @@
-angular.module('appLibranzaLiquidador', [])
-.controller("libranzaLiquidadorCtrl", function($scope, $http) {
-	$scope.lineaCredito = [
-		{
-			label: 'Libre inversión',
-			value: 'Libre inversion'
-		},
-		{
-			label: 'Libre inversión + Compra de cartera',
-			value: 'Libre inversion + Compra de cartera'
-		}
-	];
+app.controller("libranzaLiquidadorCtrl", function($scope, $http) {
+	
 	$scope.cities = [
 		{ label : 'ARMENIA',value: 'ARMENIA' },
 		{ label : 'MANIZALES',value: 'MANIZALES' },
@@ -129,42 +119,56 @@ angular.module('appLibranzaLiquidador', [])
 		}
 	};
 
+
+	$scope.lines=[];
+	$scope.pagaduriaLibranza=[];
+	$scope.libranzaProfiles=[];
+
+	$scope.getData=function(){
+		$http({
+			method:'GET',
+			url:'api/getDataLibranza'
+		}).then(function successCallback(response){
+			if(response.data != false){
+				
+				angular.forEach(response.data.lines, function(value, key) {
+					$scope.lines.push(value);
+				});
+
+				angular.forEach(response.data.profiles, function(value, key) {
+					$scope.libranzaProfiles.push(value);
+				});
+			}
+
+			console.log($scope.pagaduriaLibranza);
+		},function errorCallback(response){
+			
+		});
+	}
+
+	$scope.assignPagaduria = function(){
+
+	}
+
 	$scope.selectPagaduria = function (){
-		$scope.pagaduriasPensionados = [
-			{label: 'ACALDIA DE IBAGUE',value: 'ACALDIA DE IBAGUE'},{label: 'ALCALDIA DE PEREIRA',value: 'ALCALDIA DE PEREIRA'},{label: 'ALCALDIA DE MANIZALES',value: 'ALCALDIA DE MANIZALES'},{label: 'ALCALDIA DE SINCELEJO',value: 'ALCALDIA DE SINCELEJO'},{label: 'BBVA SEGUROS ',value: 'BBVA SEGUROS '},{label: 'CASUR',value: 'CASUR'},{label: 'COLFONDOS',value: 'COLFONDOS'},{label: 'COLPENSIONES',value: 'COLPENSIONES'},{label: 'FOPEP',value: 'FOPEP'},{label: 'EMPOS -COLPENSIONES',value: 'EMPOS -COLPENSIONES'},{label: 'GOBERNACIÓN DE BOLIVAR',value: 'GOBERNACIÓN DE BOLIVAR'},{label: 'GOBERNACION DE CALDAS',value: 'GOBERNACION DE CALDAS'},{label: 'GOBERACION DE RISARALDA',value: 'GOBERACION DE RISARALDA'},{label: 'GOBERNACION QUINDIO',value: 'GOBERNACION QUINDIO'},{label: 'MAPFRE',value: 'MAPFRE'},{label: 'MIN DEFENSA',value: 'MIN DEFENSA'},{label: 'PORVENIR',value: 'PORVENIR'},{label: 'POSITIVA',value: 'POSITIVA'},{label: 'FIDUPREVISORA',value: 'FIDUPREVISORA'},{label: 'PROTECCION', value: 'PROTECCION'},{label: 'SEGUROS DE VIDA SURAMEICANA',value: 'SEGUROS DE VIDA SURAMEICANA'},{label: 'SEGUROS BOLIVAR RENTA VITALICIA',value: 'SEGUROS BOLIVAR RENTA VITALICIA'},{label: 'GLOBAL SEGUROS',value: 'GLOBAL SEGUROS'},{label: 'AXA COLPATRIA RENTA VITALICIA',value: 'AXA COLPATRIA RENTA VITALICIA'},{label: 'FONCEP',value: 'FONCEP'}
-		];
 
-		$scope.pagaduriasDocentes = [
-			{label: 'ACALDIA DE IBAGUE',value: 'ACALDIA DE IBAGUE'},
-			{label: 'ALCALDIA DE PEREIRA',value: 'ALCALDIA DE PEREIRA'},
-			{label: 'ALCALDIA DE MANIZALES',value: 'ALCALDIA DE MANIZALES'},
-			{label: 'ALCALDIA DE SINCELEJO',value: 'ALCALDIA DE SINCELEJO'},
-			{label: 'ALCALDIA MAYOR DE BOGOTA',value: 'ALCALDIA MAYOR DE BOGOTA'},
-			{label: 'FER BOYACA',value: 'FER BOYACA'},
-			{label: 'GOBERNACION DE BOLIVAR',value: 'GOBERNACION DE BOLIVAR'},
-			{label: 'GOBERNACION DE RISARALDA',value: 'GOBERNACION DE RISARALDA'},
-			{label: 'GOBERNACION DEL QUINDIO',value: 'GOBERNACION DEL QUINDIO'}
-		];
-
-		$scope.pagaduriasMilitares = [
-			{label: 'MINISTERIO DE DEFENSA NACIONAL',value: 'MINISTERIO DE DEFENSA NACIONAL'}
-		];
-
-		if($scope.libranza.customerType == 'Pensionado'){
-			$scope.libranza.pagaduria = "ACALDIA DE IBAGUE";
-			$scope.pagadurias = $scope.pagaduriasPensionados;
-		}else if($scope.libranza.customerType == 'Docente'){
-			$scope.libranza.pagaduria = "ACALDIA DE IBAGUE";
-			$scope.pagadurias = $scope.pagaduriasDocentes;
-		}else{
-			$scope.libranza.pagaduria = "MINISTERIO DE DEFENSA NACIONAL";
-			$scope.pagadurias = $scope.pagaduriasMilitares;
-		}
+		$http({
+			method:'GET',
+			url:'/api/getPagadurias/'+$scope.libranza.customerType
+		}).then(function successCallback(response){
+			console.log(typeof $scope.libranza.customerType);
+			angular.forEach(response.data, function(value, key) {
+				$scope.pagaduriaLibranza.push(value);
+			});
+		},function errorCallback(response){	
+			console.log(response);
+		});
+		
 	};
 
 	$scope.calculateData = function(){
 		$scope.libranza.lawDesc = $scope.libranza.salary * 0.12;
-		$scope.libranza.segMargen = ($scope.libranza.salary > 781242) ? 5300 : 2000 ;
+		$scope.libranza.segMargen = ($scope.libranza.salary > 828116) ? 5300 : 2000 ;
 		$scope.libranza.quaotaAvailable = (($scope.libranza.salary - $scope.libranza.lawDesc)/2)-$scope.libranza.otherDesc-$scope.libranza.segMargen-$scope.libranza.quotaBuy;
 		if($scope.libranza.age >= 18 && $scope.libranza.age < 80){
 			$scope.libranza.maxQuota = 60000000;
@@ -219,4 +223,7 @@ angular.module('appLibranzaLiquidador', [])
 			});
 		}
 	};
+	
+	$scope.getData();
+
 });
