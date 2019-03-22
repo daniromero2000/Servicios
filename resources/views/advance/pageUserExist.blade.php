72c4cf6a2ff4338224f8590a92c5e437c7bf1e1f
Juan Sebastian Ormaza Quirama <juan.ormaza@laso.local>
2019-03-22 11:46:32 -0500

    Pagina usuario existente

---
+++ resources/views/advance/pageUserExist.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.app')
+
+@section('content')
+	<div class="row resetRow">
+		<div class="col-12 text-center resetCol thankContainer">
+			<div class="containerThankPage">	
+				<img src="{{ asset('images/imageThankPage.jpg')}}" class="img-fluid">
+			</div>
+			<div class="dialogThakPage">	
+				<img src="{{ asset('images/dialogThankPage.png')}}" class="img-fluid">
+				<p> Actualmente ya cuentas con una solicitud que está siendo procesada.</p>
+			</div>
+		</div>
+	</div>
+@endsection()
