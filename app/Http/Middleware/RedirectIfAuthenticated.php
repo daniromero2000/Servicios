d79f81d22e7d357511d85f1aba8750f6e1e3f418
Juan Sebastian Ormaza Quirama <juan.ormaza@laso.local>
2019-03-26 11:18:22 -0500

    Redireccion cuando esta logueado

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -30,7 +30,7 @@ class RedirectIfAuthenticated
             
             default:
                 if (Auth::guard($guard)->check()) {
-                    return redirect('/dashboard');
+                    return redirect('/Administrator/dashboard');
                 }
                 break;
         }
