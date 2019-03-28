f6bc33469f76f282c11878f85c363a76d6419c4c
Robert Sneyder García Moreno <robert.garcia@laso.local>
2019-03-28 16:31:14 -0500

    simulator fixes

---
+++ app/CiudadesSoc.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class CiudadesSoc extends Model
+{
+    protected $table = 'ciudades_soc';
+
+    protected $fillable = ['office', 'address', 'city', 'state', 'responsable', 'phone'];
+}
