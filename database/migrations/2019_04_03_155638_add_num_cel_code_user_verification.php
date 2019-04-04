092d2ee231640346718db5877081c2f3e0897107
Juan Sebastian Ormaza Quirama <juan.ormaza@laso.local>
2019-04-04 08:21:40 -0500

    delete folders and files

--- database/migrations/2019_04_03_155638_add_num_cel_code_user_verification.php
+++ database/migrations/2019_04_03_155638_add_num_cel_code_user_verification.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AlterAddIdCampaignImages extends Migration
+class AddNumCelCodeUserVerification extends Migration
 {
     /**
      * Run the migrations.
@@ -13,8 +13,8 @@ class AlterAddIdCampaignImages extends Migration
      */
     public function up()
     {
-        Schema::table('campaign_images',function(Blueprint $table){
-            $table->increments('id');
+        Schema::table('code_user_verification', function (Blueprint $table) {
+            //
         });
     }
 
@@ -25,6 +25,8 @@ class AlterAddIdCampaignImages extends Migration
      */
     public function down()
     {
-        //
+        Schema::table('code_user_verification', function (Blueprint $table) {
+            //
+        });
     }
 }
