a0eae3b565fb084d345d56113c657aedf6086c7f
desarrollo <desarrollo1@lagobo.com>
2019-06-05 15:49:11 -0500

    Mensaje final cambiado

--- resources/views/oportuya/thankYouPageDenied.blade.php
+++ resources/views/oportuya/thankYouPageDenied.blade.php
@@ -1,14 +1,26 @@
 @extends('layouts.app')
 
 @section('content')
-	<div class="row resetRow">
-		<div class="col-12 text-center resetCol thankContainer">
-			<div class="containerThankPage">	
-				<img src="{{ asset('images/imageThankPage.jpg')}}" class="img-fluid">
-			</div>
-			<div class="dialogThakPage">	
-				<img src="{{ asset('images/dialogThankPage.png')}}" class="img-fluid">
-				<p>	Estimado usuario, por políticas internas de la organización no es posible continuar con el proceso de crédito en Almacenes Oportunidades.</p>
+	<div id="mensajeFinal">
+		<div class="max-width-content-12">
+			<div class="row">
+				<div class="col-sm-10 offset-sm-1">
+					<div class="row resetRow containMensajeFinal">
+						<div class="col-sm-12 col-md-4 text-center mensajeFinal-containImg">
+							<img src="{{asset('images/imgMensajeFinal.png')}}" class="mensajeFinal-img img-fluid" alt="">
+						</div>
+						<div class="col-sm-12 col-md-8 text-center" style="position:relative">
+							<div class="mensajeFinal-containText">
+								<p class="mensajeFinal-text">
+									Si tu crédito no fue aprobado, no te preocupes,<br> aún tenemos más <strong>Oportunidades</strong> de crédito para ti
+								</p>
+								<p class="mensajeFinal-text2">
+									Un asesor se contactará contigo <br> para ofrecerte más información.
+								</p>
+							</div>
+						</div>
+					</div>
+				</div>
 			</div>
 		</div>
 	</div>
