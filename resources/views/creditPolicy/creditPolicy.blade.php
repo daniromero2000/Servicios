199be6cbb5327db915e4c7cddd07424a2ac0c6dd
desarrollo <desarrollo1@lagobo.com>
2019-07-17 15:24:57 -0500

    Simular individual, formato de pesos y tipo de documento

--- resources/views/creditPolicy/creditPolicy.blade.php
+++ resources/views/creditPolicy/creditPolicy.blade.php
@@ -74,10 +74,18 @@
             <div class="row">
                 <div class="col-sm-12 col-md-6">
                    <p>
-                       <label for="">Tipo de documento: </label> @{{ infoLead.TIPO_DOC }}
+                       <label for="">Tipo de documento: </label>
+                       <span ng-if="infoLead.TIPO_DOC == 1">Cédula de ciudadanía</span>
+                       <span ng-if="infoLead.TIPO_DOC == 2">NIT</span>
+                       <span ng-if="infoLead.TIPO_DOC == 3">Cédula de extranjería</span>
+                       <span ng-if="infoLead.TIPO_DOC == 4">Tarjeta de identidad</span>
+                       <span ng-if="infoLead.TIPO_DOC == 5">Pasaporte</span>
+                       <span ng-if="infoLead.TIPO_DOC == 6">Tarjeta seguro social extranjero</span>
+                       <span ng-if="infoLead.TIPO_DOC == 7">Sociedad extranjera sin NIT en Colombia</span>
+                       <span ng-if="infoLead.TIPO_DOC == 8">Fidecoismo</span>
                     </p>
                    <p>
-                       <label for="">Nmero de documento: </label>@{{ infoLead.CEDULA }}
+                       <label for="">Número de documento: </label>@{{ infoLead.CEDULA }}
                     </p>
                    <p>
                        <label for="">Tipo de cliente: </label>@{{ infoLead.TIPO_CLIENTE }}
@@ -98,10 +106,10 @@
                         <label for="">Tiempo Labor: </label><span ng-if="infoLead.TIEMPO_LABOR == 1">Si cumple</span> <span ng-if="infoLead.TIEMPO_LABOR == 0">No cumple</span>
                     </p>
                     <p ng-if="infoLead.ACTIVIDAD == 'NO CERTIFICADO' || infoLead.ACTIVIDAD == 'INDEPENDIENTE CERTIFICADO' || infoLead.ACTIVIDAD == 'RENTISTA'">
-                        <label for="">Ingresos: </label><span>@{{ infoLead.SUELDOIND + infoLead.OTROS_ING }}</span>
+                        <label for="">Ingresos: </label><span>$ @{{ infoLead.SUELDOIND + infoLead.OTROS_ING | number:0}}</span>
                     </p>
                     <p ng-if="infoLead.ACTIVIDAD == 'EMPLEADO' || infoLead.ACTIVIDAD == 'PENSIONADO' || infoLead.ACTIVIDAD == 'SOLDADO-MILITAR-POLICÍA' || infoLead.ACTIVIDAD == 'PRESTACIÓN DE SERVICIOS'">
-                        <label for="">Ingresos: </label><span>@{{ infoLead.SUELDO + infoLead.OTROS_ING }}</span>
+                        <label for="">Ingresos: </label><span>$@{{ infoLead.SUELDO + infoLead.OTROS_ING | number:0 }}</span>
                     </p>
                 </div>
                 <div class="col-sm-12 col-md-6">
