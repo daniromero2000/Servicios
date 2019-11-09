727b2c53078ee442f5375ebc69d4ed351f757b75
Dalkyel <j.alejandrocr@gmail.com>
2019-11-08 19:24:47 -0500

    08oct19, module refactoring

--- app/Http/Controllers/Admin/ViajesController.php
+++ app/Http/Controllers/Admin/ViajesController.php
@@ -3,62 +3,26 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Imagenes;
-use App\Lead;
 use App\Viajes;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Entities\Leads\Repositories\Interfaces\LeadRepositoryInterface;
+use App\Entities\Subsidiaries\Repositories\Interfaces\SubsidiaryRepositoryInterface;
 
 class ViajesController extends Controller
 {
+    private $leadInterface, $subsidiaryInterface;
+
+    public function __construct(
+        LeadRepositoryInterface $leadRepositoryInterface,
+        SubsidiaryRepositoryInterface $subsidiaryRepositoryInterface
+    ) {
+        $this->leadInterface       = $leadRepositoryInterface;
+        $this->subsidiaryInterface = $subsidiaryRepositoryInterface;
+    }
+
     public function index()
     {
-        $cities = [
-            ['label' => 'ARMENIA', 'value' => 'ARMENIA'],
-            ['label' => 'MANIZALES', 'value' => 'MANIZALES'],
-            ['label' => 'SINCELEJO', 'value' => 'SINCELEJO'],
-            ['label' => 'YOPAL', 'value' => 'YOPAL'],
-            ['label' => 'CERETÉ', 'value' => 'CERETÉ'],
-            ['label' => 'TULUÁ', 'value' => 'TULUÁ'],
-            ['label' => 'ACACÍAS', 'value' => 'ACACÍAS'],
-            ['label' => 'ESPINAL', 'value' => 'ESPINAL'],
-            ['label' => 'MARIQUITA', 'value' => 'MARIQUITA'],
-            ['label' => 'CARTAGENA', 'value' => 'CARTAGENA'],
-            ['label' => 'LA DORADA', 'value' => 'LA DORADA'],
-            ['label' => 'IBAGUÉ', 'value' => 'IBAGUÉ'],
-            ['label' => 'MONTERÍA', 'value' => 'MONTERÍA'],
-            ['label' => 'MAGANGUÉ', 'value' => 'MAGANGUÉ'],
-            ['label' => 'PEREIRA', 'value' => 'PEREIRA'],
-            ['label' => 'CALI', 'value' => 'CALI'],
-            ['label' => 'MONTELIBANO', 'value' => 'MONTELIBANO'],
-            ['label' => 'SAHAGÚN', 'value' => 'SAHAGÚN'],
-            ['label' => 'PLANETA RICA', 'value' => 'PLANETA RICA'],
-            ['label' => 'COROZAL', 'value' => 'COROZAL'],
-            ['label' => 'CIÉNAGA', 'value' => 'CIÉNAGA'],
-            ['label' => 'MONTELÍ', 'value' => 'MONTELÍ'],
-            ['label' => 'PLATO', 'value' => 'PLATO'],
-            ['label' => 'SABANALARGA', 'value' => 'SABANALARGA'],
-            ['label' => 'GRANADA', 'value' => 'GRANADA'],
-            ['label' => 'PUERTO BERRÍ', 'value' => 'PUERTO BERRÍ'],
-            ['label' => 'VILLAVICENCIO', 'value' => 'VILLAVICENCIO'],
-            ['label' => 'TAURAMENA', 'value' => 'TAURAMENA'],
-            ['label' => 'PUERTO GAITÁN', 'value' => 'PUERTO GAITÁN'],
-            ['label' => 'PUERTO BOYACÁ', 'value' => 'PUERTO BOYACÁ'],
-            ['label' => 'PUERTO LÓPEZ', 'value' => 'PUERTO LÓPEZ'],
-            ['label' => 'SEVILLA', 'value' => 'SEVILLA'],
-            ['label' => 'CHINCHINÁ', 'value' => 'CHINCHINÁ'],
-            ['label' => 'AGUACHICA', 'value' => 'AGUACHICA'],
-            ['label' => 'BARRANCABERMEJA', 'value' => 'BARRANCABERMEJA'],
-            ['label' => 'LA VIRGINIA', 'value' => 'LA VIRGINIA'],
-            ['label' => 'SANTA ROSA DE CABAL', 'value' => 'SANTA ROSA DE CABAL'],
-            ['label' => 'GIRARDOT', 'value' => 'GIRARDOT'],
-            ['label' => 'VILLANUEVA', 'value' => 'VILLANUEVA'],
-            ['label' => 'PITALITO', 'value' => 'PITALITO'],
-            ['label' => 'GARZÓN', 'value' => 'GARZÓN'],
-            ['label' => 'NEIVA', 'value' => 'NEIVA'],
-            ['label' => 'LORICA', 'value' => 'LORICA'],
-            ['label' => 'AGUAZUL',  'value' => 'AGUAZUL']
-        ];
-
         $plans = Viajes::selectRaw('imagenes.img,viajes.destination,viajes.beginDate,viajes.endingDate,viajes.isLocal,viajes.price,viajes.description,viajes.textButton')
             ->leftjoin('imagenes', 'viajes.idImg', '=', 'imagenes.id')
             ->where('imagenes.category', '=', '2')
@@ -72,7 +36,7 @@ class ViajesController extends Controller
 
         return view('viajes.index', [
             'images'       => Imagenes::all(),
-            'cities'       => $cities,
+            'cities'       => $this->subsidiaryInterface->getAllSubsidiaryCityNames(),
             'imagesViajes' => $imagesViajes,
             'plans'        => $plans
         ]);
@@ -83,31 +47,8 @@ class ViajesController extends Controller
 
     public function store(Request $request)
     {
-        $lead = new Lead;
-        $lead->name = $request->get('name');
-        $lead->lastName = $request->get('lastName');
-        $lead->email = $request->get('email');
-        $lead->telephone = $request->get('telephone');
-        $lead->city = $request->get('city');
-        $lead->typeService = $request->get('typeService');
-        $lead->typeProduct = $request->get('typeProduct');
-        $lead->state = intval($request->get('state'));
-        $lead->channel = intval($request->get('channel'));
-        $lead->termsAndConditions = $request->get('termsAndConditions');
-        $lead->save();
+        $this->leadInterface->createLead($request->input());
 
         return redirect()->route('thankYouPageViajes');
     }
-
-    public function show($id)
-    { }
-
-    public function edit($id)
-    { }
-
-    public function update(Request $request, $id)
-    { }
-
-    public function destroy($id)
-    { }
 }
