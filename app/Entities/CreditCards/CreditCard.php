8778fb12210028626f0ac3d1dbbe4c7390fb9768
Dalkyel <j.alejandrocr@gmail.com>
2019-11-13 22:06:58 -0500

    13oct19, creacion modelos

--- app/Entities/CreditCards/CreditCard.php
+++ app/Entities/CreditCards/CreditCard.php
@@ -11,6 +11,51 @@ class CreditCard extends Model
 
     public $connection = 'oportudata';
 
+    protected $fillable = [
+        'NUMERO',
+        'SOLICITUD',
+        'CLIENTE',
+        'APROBACION',
+        'DESPACHO',
+        'LOTE',
+        'FEC_APROB',
+        'CUOTA_MAN',
+        'CARGO',
+        'CUP_INICIA',
+        'CUP_COMPRA',
+        'COMPRA_ACT',
+        'COMPRA_EFE',
+        'CUPO_EFEC',
+        'CUP_ACTUAL',
+        'CUPOMAX',
+        'SUC',
+        'ESTADO',
+        'FEC_ACTIV',
+        'CONS',
+        'OPORTUNID',
+        'EXTRACUPO',
+        'EXTRA_ACT',
+        'RECEPC1',
+        'RECEPC2',
+        'RECEPC3',
+        'FEC_REC',
+        'OBSTAR1',
+        'OBSTAR2',
+        'OBSTAR3',
+        'TIPO_TAR',
+        'RESPUEST',
+        'RECEPCOFI',
+        'OBSTAROFI',
+        'FEC_RECOFI',
+        'RECEPCSUC',
+        'OBSTARSUC',
+        'FEC_RECSUC',
+        'RECEPCCLI',
+        'OBSTARCLI',
+        'FEC_RECCLI',
+        'STATE'
+    ];
+
     public function customer()
     {
         return $this->belongsTo(Customer::class);
