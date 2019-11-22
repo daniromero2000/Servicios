db464a687bcc646d6961e153178c105cba3f7486
Alejandro Castañeda <desarrollo1@lagobo.com>
2019-11-22 07:42:07 -0500

    22nov19, models refactoring

--- app/Http/Controllers/Front/Advances/AdvanceController.php
+++ app/Http/Controllers/Front/Advances/AdvanceController.php
@@ -45,51 +45,51 @@ class AdvanceController extends Controller
     public function index()
     {
 
-        $identificationNumber = 1088019814;
+        // $identificationNumber = 1088019814;
 
-        //3.5 Historial de Crédito
-        $historialCrediticio = 0;
-        $totalVector = 0;
-
-
-        $queryComporFin = sprintf("SELECT fdcompor, fdapert
-        FROM cifin_findia
-        WHERE fdcalid = 'PRIN' AND `fdconsul` = (SELECT MAX(`fdconsul`) FROM `cifin_findia` WHERE `fdcedula` = %s ) AND fdcedula = %s", $identificationNumber, $identificationNumber);
-        $respQueryComporFin = DB::connection('oportudata')->select($queryComporFin);
-
-        // $respQueryComporFin =  $this->upToDate->checkCustomerHasUpToDateCifin6($identificationNumber);
-        foreach ($respQueryComporFin as $value) {
-            $totalVector = 0;
-            if ($value->fdapert == '') {
-                break;
-            }
-            $fechaComporFin = $value->fdapert;
-            $fechaComporFin = explode('/', $fechaComporFin);
-            $fechaComporFin = $fechaComporFin[2] . "-" . $fechaComporFin[1] . "-" . $fechaComporFin[0];
-            $dateNow = date('Y-m-d');
-            $dateNew = strtotime("- 24 MONTH", strtotime($dateNow));
-            if (strtotime($fechaComporFin) > $dateNew) {
-                $paymentArray = explode('|', $value->fdcompor);
-                $paymentArray = array_map(array($this, 'applyTrim'), $paymentArray);
-                $popArray = array_pop($paymentArray);
-                $paymentArray = array_reverse($paymentArray);
-                foreach ($paymentArray as $habit) {
-                    if ($totalVector >= 6) { // Poner parametrizable
-                        $historialCrediticio = 1;
-                        break;
-                    }
-
-                    if ($habit == 'N') {
-                        $totalVector++;
-                    } else {
-                        $totalVector = 0;
-                    }
-                }
-            }
-        }
+        // //3.5 Historial de Crédito
+        // $historialCrediticio = 0;
+        // $totalVector = 0;
 
 
-        dd($totalVector);
+        // $queryComporFin = sprintf("SELECT fdcompor, fdapert
+        // FROM cifin_findia
+        // WHERE fdcalid = 'PRIN' AND `fdconsul` = (SELECT MAX(`fdconsul`) FROM `cifin_findia` WHERE `fdcedula` = %s ) AND fdcedula = %s", $identificationNumber, $identificationNumber);
+        // $respQueryComporFin = DB::connection('oportudata')->select($queryComporFin);
+
+        // // $respQueryComporFin =  $this->upToDate->checkCustomerHasUpToDateCifin6($identificationNumber);
+        // foreach ($respQueryComporFin as $value) {
+        //     $totalVector = 0;
+        //     if ($value->fdapert == '') {
+        //         break;
+        //     }
+        //     $fechaComporFin = $value->fdapert;
+        //     $fechaComporFin = explode('/', $fechaComporFin);
+        //     $fechaComporFin = $fechaComporFin[2] . "-" . $fechaComporFin[1] . "-" . $fechaComporFin[0];
+        //     $dateNow = date('Y-m-d');
+        //     $dateNew = strtotime("- 24 MONTH", strtotime($dateNow));
+        //     if (strtotime($fechaComporFin) > $dateNew) {
+        //         $paymentArray = explode('|', $value->fdcompor);
+        //         $paymentArray = array_map(array($this, 'applyTrim'), $paymentArray);
+        //         $popArray = array_pop($paymentArray);
+        //         $paymentArray = array_reverse($paymentArray);
+        //         foreach ($paymentArray as $habit) {
+        //             if ($totalVector >= 6) { // Poner parametrizable
+        //                 $historialCrediticio = 1;
+        //                 break;
+        //             }
+
+        //             if ($habit == 'N') {
+        //                 $totalVector++;
+        //             } else {
+        //                 $totalVector = 0;
+        //             }
+        //         }
+        //     }
+        // }
+
+
+        // dd($totalVector);
 
         return view('advance.index', [
             'images' => Imagenes::selectRaw('*')->where('category', '=', '3')->where('isSlide', '=', '1')->get(),
