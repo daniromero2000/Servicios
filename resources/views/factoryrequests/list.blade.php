7fd3ce2036e9a2a4bbe9956b449af49df2f34e85
daniromero2000 <56078676+daniromero2000@users.noreply.github.com>
2019-11-27 12:46:06 -0500

    dashboard

--- resources/views/factoryrequests/list.blade.php
+++ resources/views/factoryrequests/list.blade.php
@@ -4,36 +4,37 @@
 <section>
     @include('layouts.errors-and-messages')
     @if(!is_null($customers))
-    <div >
-            <div class="content-header">
-                    <div class="container-fluid">
+    <div>
+        <div class="content-header">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-sm-8">
+                        <h1 class="ml-3"> {{ $listCount }} Solicitudes</h1>
                         <div class="row">
-                            <div class="col-sm-8">
-                                    <h1 class="ml-3"> {{ $listCount }} Solicitudes</h1>
-                                    <div class="row">
-                                        <div class="col-md-12">
-                                            @include('layouts.admin.search', ['route' => route('factoryrequests.index')])
-                                        </div>
+                            <div class="col-md-12">
 
-                                    </div>
-                            </div><!-- /.col -->
-                            <div class="col-sm-6">
-                                <ol class="breadcrumb float-sm-right">
-                                    <li class="breadcrumb-item"><a href="/Administrator/dashboard">Dashboard</a></li>
-                                    <li class="breadcrumb-item active"><a href="/Administrator/communityLeads#!/"></a></li>
-                                </ol>
-                            </div><!-- /.col -->
-                        </div><!-- /.row -->
-                    </div><!-- /.container-fluid -->
-                </div>
+                            </div>
+
+                        </div>
+                    </div><!-- /.col -->
+                    <div class="col-sm-6">
+                        <ol class="breadcrumb float-sm-right">
+                            <li class="breadcrumb-item"><a href="/Administrator/dashboard">Dashboard</a></li>
+                            <li class="breadcrumb-item active"><a href="/Administrator/communityLeads#!/"></a></li>
+                        </ol>
+                    </div><!-- /.col -->
+                </div><!-- /.row -->
+            </div><!-- /.container-fluid -->
+        </div>
 
         <div class="container">
             <div class="card  mb-4 border-0 shadow-lg">
                 <div class="row form-group" ng-if="filtros">
                     <div class="col-12">
-
+                        <div class="card-header">
+                            @include('layouts.admin.search', ['route' => route('factoryrequests.index')])
+                        </div>
                         <div class="card-body text-center pt-0 pb-0 ">
-
                             @if($customers)
                             @include('layouts.admin.tables.tables_lead_status', [$headers, 'datas' => $customers ])
                             @include('layouts.admin.pagination.pagination', [$skip])
--- resources/views/layouts/admin/search.blade.php
+++ resources/views/layouts/admin/search.blade.php
@@ -1,33 +1,52 @@
 <!-- search form -->
-<form action="{{$route}}" method="get" id="admin-search">
-    <div class="input-group">
-        <div class="form-group">
-            <label for="q">Texto</label>
-            <input type="text" name="q" class="form-control form-control-sm" placeholder="Buscar..."
-                value="{!! request()->input('q') !!}">
-        </div>
-        <div class="form-group">
-            <label for="status">Estado</label>
-            <select class="form-control" id="status" name="status" {!! request()->input('status') !!}>
-                <option disabled selected value> -- Selecciona Estado -- </option>
-                <option>APROBADO</option>
-                <option>ANALISIS</option>
-                <option>EN SUCURSAL</option>
-                <option>PROBLEMAS EN ANALISIS</option>
-                <option>EN SUCURSAL</option>
-                <option>PROBLEMAS EN REFERENCIACION</option>
-            </select>
-        </div>
-        <div class="form-group">
-           <label for="from">Desde</label>
-            <input type="date" name="from" class="form-control form-control-sm" value="{!! request()->input('from') !!}">
-        </div>
-        <div class="form-group">
-           <label for="to">Hasta</label>
-            <input type="date" name="to" class="form-control form-control-sm" value="{!! request()->input('to') !!}">
-        </div>
-        <span class="input-group-btn">
-            <button type="submit" id="search-btn" class="btn btn-flat"><i class="fa fa-search"></i> Buscar </button>
-        </span>
+<div class="row">
+    <div class="col-12 text-center">
+        <h3>Filtrar</h3>
     </div>
-</form>
+
+    <div class="col-12 mt-2">
+        <form action="{{$route}}" method="get" id="admin-search">
+            <div class="input-group">
+
+
+                <div class="row w-100 d-flex justify-content-center">
+                    <div class="col-3">
+                        <label for="q">Texto</label>
+                        <input type="text" name="q" class="form-control form-control-sm" placeholder="Buscar..."
+                            value="{!! request()->input('q') !!}">
+                    </div>
+                    <div class="col-3">
+                        <label for="status">Estado</label>
+                        <select class="form-control select2bs4 select2-hidden-accessible" id="status" name="status" {!! request()->input('status') !!}>
+                            <option disabled selected value> -- Selecciona Estado -- </option>
+                            <option>APROBADO</option>
+                            <option>ANALISIS</option>
+                            <option>EN SUCURSAL</option>
+                            <option>PROBLEMAS EN ANALISIS</option>
+                            <option>EN SUCURSAL</option>
+                            <option>PROBLEMAS EN REFERENCIACION</option>
+                        </select>
+                    </div>
+                    <div class="col-3">
+                        <label for="from">Desde</label>
+                        <input type="date" name="from" class="form-control form-control-sm"
+                            value="{!! request()->input('from') !!}">
+                    </div>
+                    <div class="col-3">
+                        <label for="to">Hasta</label>
+                        <input type="date" name="to" class="form-control form-control-sm"
+                            value="{!! request()->input('to') !!}">
+                    </div>
+                </div>
+
+            </div>
+            <div class="col-12 text-right">
+                <span class="input-group-btn">
+                    <button type="submit" id="search-btn" class="btn btn-flat"><i class="fa fa-search"></i> Buscar
+                    </button>
+                </span>
+            </div>
+
+    </div>
+    </form>
+</div>
