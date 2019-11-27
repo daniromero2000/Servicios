e5d514efead44a064ddccf42497905db1b6dc856
daniromero2000 <56078676+daniromero2000@users.noreply.github.com>
2019-11-27 15:14:02 -0500

    dashboard

--- resources/views/layouts/admin/search.blade.php
+++ resources/views/layouts/admin/search.blade.php
@@ -1,7 +1,7 @@
 <!-- search form -->
 <div class="row">
     <div class="col-12 text-center">
-        <h3>Filtrar</h3>
+        <h3 style="color: #007bff;">Filtrar</h3>
     </div>
 
     <div class="col-12 mt-2">
@@ -11,38 +11,40 @@
 
                 <div class="row w-100 d-flex justify-content-center">
                     <div class="col-3">
-                        <label for="q">Texto</label>
-                        <input type="text" name="q" class="form-control form-control-sm" placeholder="Buscar..."
+                        <label for="q">Buscar</label>
+                        <input type="text" name="q" class="form-control"  placeholder=" Buscar..."
                             value="{!! request()->input('q') !!}">
                     </div>
                     <div class="col-3">
-                        <label for="status">Estado</label>
-                        <select class="form-control select2bs4 select2-hidden-accessible" id="status" name="status" {!! request()->input('status') !!}>
-                            <option disabled selected value> -- Selecciona Estado -- </option>
-                            <option>APROBADO</option>
-                            <option>ANALISIS</option>
-                            <option>EN SUCURSAL</option>
-                            <option>PROBLEMAS EN ANALISIS</option>
-                            <option>EN SUCURSAL</option>
-                            <option>PROBLEMAS EN REFERENCIACION</option>
-                        </select>
+                        <div class="form-group">
+                            <label for="status">Estado</label>
+                            <select class="form-control  select2" id="status" name="status" {!! request()->input('status') !!} style="width: 100%;">
+                                <option disabled selected value> -- Selecciona Estado -- </option>
+                                <option>APROBADO</option>
+                                <option>ANALISIS</option>
+                                <option>EN SUCURSAL</option>
+                                <option>PROBLEMAS EN ANALISIS</option>
+                                <option>EN SUCURSAL</option>
+                                <option>PROBLEMAS EN REFERENCIACION</option>
+                            </select>
+                        </div>
                     </div>
                     <div class="col-3">
                         <label for="from">Desde</label>
-                        <input type="date" name="from" class="form-control form-control-sm"
+                        <input type="date" name="from" class="form-control "
                             value="{!! request()->input('from') !!}">
                     </div>
                     <div class="col-3">
                         <label for="to">Hasta</label>
-                        <input type="date" name="to" class="form-control form-control-sm"
+                        <input type="date" name="to" class="form-control "
                             value="{!! request()->input('to') !!}">
                     </div>
                 </div>
 
             </div>
             <div class="col-12 text-right">
-                <span class="input-group-btn">
-                    <button type="submit" id="search-btn" class="btn btn-flat"><i class="fa fa-search"></i> Buscar
+                <span class="input-group-btn btn-pr">
+                    <button type="submit" id="search-btn" class="btn btn-primary"><i class="fa fa-search"></i> Buscar
                     </button>
                 </span>
             </div>
