0694c8c7de208e6e30f59479278a4206fe9dd23b
Alejandro Castañeda <desarrollo1@lagobo.com>
2019-11-29 15:28:10 -0500

    29nov19, intentions Reports

--- routes/web2.php
+++ routes/web2.php
@@ -142,7 +142,7 @@ Route::namespace('Admin')->group(function () {
 
     Route::namespace('Intentions')->group(function () {
         Route::resource('intentions', 'IntentionController');
-        Route::get('intentions/dashboard/intentions', 'IntentionController@dashboard')->name('intention_dashboard');
+        Route::get('dashboard/intentions', 'IntentionController@dashboard')->name('intention_dashboard');
     });
 
     Route::namespace('Comments')->group(function () {
