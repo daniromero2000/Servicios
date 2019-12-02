cf22872fd8f49d10bf2f61f535d9f732cbe9993d
Alejandro Castañeda <desarrollo1@lagobo.com>
2019-12-02 09:18:23 -0500

    02dic19, modules addition

--- resources/views/factoryrequests/dashboard.blade.php
+++ resources/views/factoryrequests/dashboard.blade.php
@@ -190,6 +190,7 @@
       <!-- TORTA -->
       <div class="card">
         <div class="card-header">
+          <h3 class="card-title">Estados Solicitudes</h3>
           <div class="card-tools">
             <button type="button" class="btn btn-tool" data-card-widget="collapse"><i class="fas fa-minus"></i>
             </button>
