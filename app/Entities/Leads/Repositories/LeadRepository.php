83cf2ed04ab2ee28654e831c41bc75e61c200046
Alejandro Castañeda <desarrollo1@lagobo.com>
2019-12-03 10:28:31 -0500

    03dic19, community manager dashboard

--- app/Entities/Leads/Repositories/LeadRepository.php
+++ app/Entities/Leads/Repositories/LeadRepository.php
@@ -6,9 +6,29 @@ use App\Entities\Leads\Lead;
 use App\Entities\Leads\Repositories\Interfaces\LeadRepositoryInterface;
 use Illuminate\Database\QueryException;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Collection as Support;
 
 class LeadRepository implements LeadRepositoryInterface
 {
+
+    private $columns = [
+        'id',
+        'CEDULA',
+        'FECHA_INTENCION',
+        'ID_DEF',
+        'ESTADO_OBLIGACIONES',
+        'PERFIL_CREDITICIO',
+        'HISTORIAL_CREDITO',
+        'TARJETA',
+        'ZONA_RIESGO',
+        'EDAD',
+        'TIEMPO_LABOR',
+        'TIPO_5_ESPECIAL',
+        'INSPECCION_OCULAR',
+        'ESTADO_INTENCION',
+    ];
+
+
     public function __construct(
         lead $lead
     ) {
@@ -68,7 +88,20 @@ class LeadRepository implements LeadRepositoryInterface
         try {
             return  $this->model->with('leadStatus')
                 ->whereBetween('created_at', [$from, $to])
-                ->get()->groupBy('leadStatus.status');
+                ->get(['state'])->groupBy('leadStatus.status');
+        } catch (QueryException $e) {
+            dd($e);
+        }
+    }
+
+    public function listleads($totalView): Support
+    {
+        try {
+            return  $this->model->with(['leadStatus'])
+                ->orderBy('id', 'desc')
+                ->skip($totalView)
+                ->take(30)
+                ->get($this->columns);
         } catch (QueryException $e) {
             dd($e);
         }
