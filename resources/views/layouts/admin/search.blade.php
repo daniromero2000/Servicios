9aeaa2278a0c9a0aecd87d9c2f20695dbd85670d
daniromero2000 <56078676+daniromero2000@users.noreply.github.com>
2019-12-05 18:01:47 -0500

    leads table

--- resources/views/layouts/admin/search.blade.php
+++ resources/views/layouts/admin/search.blade.php
@@ -4,18 +4,18 @@
         <h3 style="color: #007bff;">Filtrar</h3>
     </div>
 
-    <div class="col-12 mt-2">
+    <div class="col-12  mt-2">
         <form action="{{$route}}" method="get" id="admin-search">
             <div class="input-group">
 
 
                 <div class="row w-100 d-flex justify-content-center">
-                    <div class="col-3">
+                    <div class="col-3 col-sm-4">
                         <label for="q">Buscar: Solicitud / Cedula</label>
                         <input type="text" name="q" class="form-control" placeholder=" Buscar..."
                             value="{!! request()->input('q') !!}">
                     </div>
-                    <div class="col-3">
+                    <div class="col-3 col-sm-4">
                             <div class="form-group">
                                     <label for="status">Estado</label>
                                     <select class="form-control  select2bs4" id="status" name="status" {!!
@@ -47,15 +47,15 @@
                                 </div>
                     </div>
 
-                    <div class="col-3">
+                    <div class="col-3 col-sm-4">
                         <label for="from">Desde</label>
                         <input type="date" name="from" class="form-control " value="{!! request()->input('from') !!}">
                     </div>
-                    <div class="col-3">
+                    <div class="col-3 col-sm-4">
                         <label for="to">Hasta</label>
                         <input type="date" name="to" class="form-control " value="{!! request()->input('to') !!}">
                     </div>
-                    <div class="col-3 d-flex align-items-end">
+                    <div class="col-3 col-sm-4 d-flex align-items-end">
                         <div class="form-group w-100">
                             <label for="subsidiary">Sucursal</label>
                             <select class="form-control  select2bs4" id="subsidiary" name="subsidiary" {!!
@@ -67,7 +67,7 @@
                             </select>
                         </div>
                     </div>
-                    <div class="col-3 d-flex align-items-center">
+                    <div class="col-3 col-sm-4 d-flex align-items-center">
                         <span class="input-group-btn btn-pr">
                             <button type="submit" id="search-btn" class="btn btn-primary mt-2"><i class="fa fa-search"></i>
                                 Buscar
