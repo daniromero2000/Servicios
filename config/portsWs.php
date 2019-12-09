ab64bf27fb00714adcc5da14fa8ef57c4f0ca2a7
Juan Sebastian Ormaza <desarrollo@lagobo.com>
2019-12-09 07:51:35 -0500

    Consumo web service pagos

--- config/portsWs.php
+++ config/portsWs.php
@@ -3,6 +3,6 @@
 return [
     'creditVision' => env('CREDITVISION_PORT'),
     'ubica' => env('UBICA_PORT'),
-    'confronta' => env('CONFRONTA_PORT')
-
+    'confronta' => env('CONFRONTA_PORT'),
+    'pagosCliente' => env('PAGOSCLIENTE_PORT')
 ];
