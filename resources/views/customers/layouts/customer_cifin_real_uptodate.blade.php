ac2ce5b5a7b6cd3cee22bd88d19aa360b489b652
Alejandro Castañeda <desarrollo1@lagobo.com>
2019-12-12 11:53:07 -0500

    12dic19, factory requests show

--- resources/views/customers/layouts/customer_cifin_real_uptodate.blade.php
+++ resources/views/customers/layouts/customer_cifin_real_uptodate.blade.php
@@ -8,6 +8,7 @@
     <table class="table table-head-fixed table-hover  table-stripped leadTable">
       <thead class="header-table">
         <tr>
+          <th class="text-center" scope="col">Codigo</th>
           <th class="text-center" scope="col">Entidad</th>
           <th class="text-center" scope="col">Estado</th>
           <th class="text-center" scope="col">Saldo Inicial</th>
@@ -20,6 +21,7 @@
       <tbody class="body-table">
         @foreach ($cifin_uptodate_reals as $cifin_uptodate_real )
         <tr>
+          <td class="text-center">{{ $cifin_uptodate_real->rdnumob }}</td>
           <td class="text-center">{{ $cifin_uptodate_real->rdnoment }}</td>
           <td class="text-center">{{ $cifin_uptodate_real->rdestob }}</td>
           <td class="text-center">{{ number_format (($cifin_uptodate_real->rdvrinic*1000)) }}</td>
