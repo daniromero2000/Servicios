f3e2d178296b43205aa54d84a801e3cdc0cca913
Alejandro Castañeda <desarrollo1@lagobo.com>
2019-12-14 10:47:19 -0500

    14dic19, fosyga reports

--- resources/views/customers/dashboard.blade.php
+++ resources/views/customers/dashboard.blade.php
@@ -137,7 +137,7 @@
       </div>
       <div class="card">
         <div class="card-header">
-          <h3 class="card-title"> {{ $totalFosygas}} Consultas Fosyga</h3>
+          <h3 class="card-title"> {{ $totalFosygas}} Consultas Fosyga<br> ¿Falló Consulta?</h3>
           <div class="card-tools">
             <button type="button" class="btn btn-tool" data-card-widget="collapse"><i class="fas fa-minus"></i>
             </button>
@@ -182,7 +182,7 @@
       </div>
       <div class="card">
         <div class="card-header">
-          <h3 class="card-title"> {{ $totalRegistradurias}} Consultas Registraduría</h3>
+          <h3 class="card-title"> {{ $totalRegistradurias}} Consultas Registraduría<br> ¿Falló Consulta?</h3>
           <div class="card-tools">
             <button type="button" class="btn btn-tool" data-card-widget="collapse"><i class="fas fa-minus"></i>
             </button>
