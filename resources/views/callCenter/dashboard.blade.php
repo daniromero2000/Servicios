42d7baab8f214ac2db03d7bd76cf73857ac0b550
Alejandro Castañeda <desarrollo1@lagobo.com>
2019-12-14 10:13:19 -0500

    14dic19, fosyga reports

--- resources/views/callCenter/dashboard.blade.php
+++ resources/views/callCenter/dashboard.blade.php
@@ -124,20 +124,6 @@
   </div>
 </div>
 @endsection
-@section('scriptsJs')
-<!-- OPTIONAL SCRIPTS -->
-<script src="{{ asset('plugins/chart.js/Chart.min.js') }}"></script>
-<script src="{{ asset('dist/js/demo.js') }}"></script>
-<script src="{{ asset('dist/js/pages/dashboard3.js') }}"></script>
-<!-- jQuery UI -->
-<script src="{{ asset('plugins/jquery-ui/jquery-ui.min.js') }}"></script>
-<!-- FLOT CHARTS -->
-<script src="{{ asset('plugins/flot/jquery.flot.js') }}"></script>
-<!-- FLOT RESIZE PLUGIN - allows the chart to redraw when the window is resized -->
-<script src="{{ asset('plugins/flot-old/jquery.flot.resize.min.js') }}"></script>
-<!-- FLOT PIE PLUGIN - also used to draw donut charts -->
-<script src="{{ asset('plugins/flot-old/jquery.flot.pie.min.js') }}"></script>
-<!-- Page script -->
+@include('layouts.admin.dashboard_imports')
 @include('callCenter.dashboardJS')
-
 @endsection
--- resources/views/customers/dashboard.blade.php
+++ resources/views/customers/dashboard.blade.php
@@ -213,17 +213,5 @@
   </div>
 </div>
 @endsection
-@section('scriptsJs')
-<script src="{{ asset('plugins/chart.js/Chart.min.js') }}"></script>
-<script src="{{ asset('dist/js/demo.js') }}"></script>
-<script src="{{ asset('dist/js/pages/dashboard3.js') }}"></script>
-<!-- jQuery UI -->
-<script src="{{ asset('plugins/jquery-ui/jquery-ui.min.js') }}"></script>
-<!-- FLOT CHARTS -->
-<script src="{{ asset('plugins/flot/jquery.flot.js') }}"></script>
-<!-- FLOT RESIZE PLUGIN - allows the chart to redraw when the window is resized -->
-<script src="{{ asset('plugins/flot-old/jquery.flot.resize.min.js') }}"></script>
-<!-- FLOT PIE PLUGIN - also used to draw donut charts -->
-<script src="{{ asset('plugins/flot-old/jquery.flot.pie.min.js') }}"></script>
-<!-- Page script -->
+@include('layouts.admin.dashboard_imports')
 @include('customers.dashboardJS')
