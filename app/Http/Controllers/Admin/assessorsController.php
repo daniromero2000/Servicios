f17dfbe885f680f5abd113a4db590483da78ccb5
Alejandro Castañeda <desarrollo1@lagobo.com>
2019-12-23 14:37:20 -0500

    23dic19, digitalchannelleads dashboear

--- app/Http/Controllers/Admin/assessorsController.php
+++ app/Http/Controllers/Admin/assessorsController.php
@@ -333,14 +333,11 @@ class assessorsController extends Controller
 
     public function dashboard(Request $request)
     {
-
         $assessor = auth()->user()->CODASESOR;
         $to       = Carbon::now();
         $from     = Carbon::now()->subMonth();
 
-        $rand  = array('0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f');
-        $color = '#' . $rand[rand(0, 15)] . $rand[rand(0, 15)] . $rand[rand(0, 15)] . $rand[rand(0, 15)] . $rand[rand(0, 15)] . $rand[rand(0, 15)];
-
+        dd($assessor);
         $estadosAssessors      = $this->factoryInterface->countAssessorFactoryRequestStatuses($from, $to, $assessor);
         $webAssessorsCounts    = $this->factoryInterface->countWebAssessorFactory($from, $to, $assessor);
         $factoryAssessorsTotal = $this->factoryInterface->getAssessorFactoryTotal($from, $to, $assessor);
