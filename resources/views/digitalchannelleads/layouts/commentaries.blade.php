001c3c9fd518a4575e1c336442b1751a399fb0d9
Alejandro Castañeda <desarrollo1@lagobo.com>
2020-01-02 11:57:03 -0500

    02jan20, digital channel reports

--- resources/views/digitalchannelleads/layouts/commentaries.blade.php
+++ resources/views/digitalchannelleads/layouts/commentaries.blade.php
@@ -1,7 +1,12 @@
-<div class="col-md-4">
+<div class="col-md-6">
     <div class="card card-table-reset">
         <div class="card-body">
             <h2 class="title-table" class="title-table"><i class="fas fa-comments" aria-hidden="true"></i> Comentarios
+                <div class="col">
+                    <a href="#myModal" data-toggle="modal" data-target="#commentmodal" <i
+                        class="btn btn-primary btn-sm"><i class="fa fa-edit"></i>
+                        Agregar Comentario</a>
+                </div>
             </h2>
             @if($datas->isNotEmpty())
             <table class="table table-hover table-stripped leadTable">
@@ -42,3 +47,5 @@
         </div>
     </div>
 </div>
+
+@include('digitalchannelleads.layouts.add_comment_modal')
