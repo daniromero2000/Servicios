4b888fa15fe06581926653877ccb0b4c9100ab1a
Alejandro Castañeda <desarrollo1@lagobo.com>
2020-01-02 11:06:58 -0500

    02jan20, digital channel reports

--- resources/views/layouts/admin/tables/table_digital_channel_leads_status.blade.php
+++ resources/views/layouts/admin/tables/table_digital_channel_leads_status.blade.php
@@ -29,8 +29,8 @@ use Carbon\Carbon;
                 <td>{{ $data->email}}</td>
                 <td>{{ $data->telephone}}</td>
                 <td>{{ $data->city}}</td>
-                <td> @if($data->leadService){{ $data->leadService->service}} @endif</td>
-                <td> @if($data->leadProduct){{ $data->leadProduct->lead_product}} @endif</td>
+                <td> @if($data->leadService){{ $data->leadService->service}} @endif {{ $data->typeService}}</td>
+                <td> @if($data->leadProduct){{ $data->leadProduct->lead_product}} @endif {{ $data->typeProduct}}</td>
                 <td>{{ $data->created_at}}</td>
                 <td>
                     <i class="fas fa-comment cursor"
