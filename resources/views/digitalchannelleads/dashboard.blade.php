c14ef1daba80b82352845c3912ae883e7a90e3c0
Alejandro Castañeda <desarrollo1@lagobo.com>
2020-01-03 11:48:20 -0500

    03jan20, digital channel reports

--- resources/views/digitalchannelleads/dashboard.blade.php
+++ resources/views/digitalchannelleads/dashboard.blade.php
@@ -135,39 +135,7 @@
         </div>
       </div>
     </div>
-    <div class=" col-md-8 col-lg-12">
-      <!-- debe ir oculta -->
-      <div hidden class="card card-danger">
-        <div class="card-header">
-          <h3 class="card-title">Donut Chart</h3>
-          <div class="card-tools">
-            <button type="button" class="btn btn-tool" data-card-widget="collapse"><i class="fas fa-minus"></i>
-            </button>
-            <button type="button" class="btn btn-tool" data-card-widget="remove"><i class="fas fa-times"></i></button>
-          </div>
-        </div>
-        <div class="card-body">
-          <canvas id="donutChart3" style="height:230px; min-height:230px"></canvas>
-        </div>
-      </div>
-      <!-- TORTA -->
-      <div class="card">
-        <div class="card-header">
-          <h3 class="card-title">Asesores</h3>
-          <div class="card-tools">
-            <button type="button" class="btn btn-tool" data-card-widget="collapse"><i class="fas fa-minus"></i>
-            </button>
-            <button type="button" class="btn btn-tool" data-card-widget="remove"><i class="fas fa-times"></i></button>
-          </div>
-        </div>
-        <div class="card-body">
-          <canvas id="pieChart3" style="height:200px; min-height:auto"></canvas>
-        </div>
-      </div>
-    </div>
-
-    <!-- Card 1 -->
-    <!-- /.col (LEFT) -->
+    @include('digitalchannelleads.layouts.pie_assessors')
 </div>
 @endsection
 @include('layouts.admin.dashboard_imports')
