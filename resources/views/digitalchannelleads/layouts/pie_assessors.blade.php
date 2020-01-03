4dd0fb011ae93d2d7fce83464acf79cc1cccf965
Alejandro Castañeda <desarrollo1@lagobo.com>
2020-01-03 11:56:09 -0500

    03jan20, digital channel reports

--- resources/views/digitalchannelleads/layouts/pie_assessors.blade.php
+++ resources/views/digitalchannelleads/layouts/pie_assessors.blade.php
@@ -16,7 +16,7 @@
   <!-- TORTA -->
   <div class="card">
     <div class="card-header">
-      <h3 class="card-title">Asesores</h3>
+      <h3 class="card-title">Leads Asesores</h3>
       <div class="card-tools">
         <button type="button" class="btn btn-tool" data-card-widget="collapse"><i class="fas fa-minus"></i>
         </button>
