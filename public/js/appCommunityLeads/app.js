1b288de77d2e15b5cafc1684aed45fa7cec2e198
Alejandro Castañeda <desarrollo1@lagobo.com>
2020-01-14 08:42:27 -0500

    14jan20, digital channel reports

--- public/js/appCommunityLeads/app.js
+++
@@ -1,6 +0,0 @@
-var app =  angular.module('communityLeadsApp',['ngRoute', 'moment-picker']);
-app.config(['$routeProvider',
-    function($routeProvider) {
-        $routeProvider.
-            when('/', { templateUrl: 'communityLeads/leads',controller: 'communityController' })
-}]);
