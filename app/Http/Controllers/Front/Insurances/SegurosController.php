6ec35b52a8cf05a989560d8110f82c361ec511eb
Juan Sebastian Ormaza <desarrollo@lagobo.com>
2020-01-16 15:50:24 -0500

    Ajuste seguros

--- app/Http/Controllers/Front/Insurances/SegurosController.php
+++ app/Http/Controllers/Front/Insurances/SegurosController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Front\Insurances;
 
+use App\Entities\Cities\Repositories\Interfaces\CityRepositoryInterface;
 use App\Imagenes;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
@@ -9,19 +10,22 @@ use App\Entities\Subsidiaries\Repositories\Interfaces\SubsidiaryRepositoryInterf
 use App\Entities\Customers\Repositories\Interfaces\CustomerRepositoryInterface;
 use App\Entities\CustomerCellPhones\Repositories\Interfaces\CustomerCellPhoneRepositoryInterface;
 use Carbon\Carbon;
-
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 class SegurosController extends Controller
 {
-    private $customerInterface, $subsidiaryInterface, $customerCellPhoneInterface;
+    private $customerInterface, $subsidiaryInterface, $customerCellPhoneInterface, $cityInterface;
 
     public function __construct(
         CustomerRepositoryInterface $customerRepositoryInterface,
         SubsidiaryRepositoryInterface $subsidiaryRepositoryInterface,
-        CustomerCellPhoneRepositoryInterface $customerCellPhoneRepositoryInterface
+        CustomerCellPhoneRepositoryInterface $customerCellPhoneRepositoryInterface,
+        CityRepositoryInterface $cityRepositoryInterface
     ) {
-        $this->subsidiaryInterface = $subsidiaryRepositoryInterface;
-        $this->customerInterface       = $customerRepositoryInterface;
-        $this->customerCellPhoneInterface        = $customerCellPhoneRepositoryInterface;
+        $this->subsidiaryInterface        = $subsidiaryRepositoryInterface;
+        $this->customerInterface          = $customerRepositoryInterface;
+        $this->customerCellPhoneInterface = $customerCellPhoneRepositoryInterface;
+        $this->cityInterface              = $cityRepositoryInterface;
     }
 
     public function index()
@@ -34,23 +38,51 @@ class SegurosController extends Controller
 
     public function store(Request $request)
     {
-        $request['FEC_ING']   = $request->input('FEC_ING')."-01";
-        $antig                = new Carbon($request['FEC_ING']);
-        $request['FEC_CONST'] = $request->input('FEC_CONST')."-01";
-        $antig_ind            = new Carbon($request['FEC_CONST']);
-        $birthday             = new Carbon($request->FEC_NAC);
-        $subsidiaryCityName   = $this->subsidiaryInterface->getSubsidiaryCityByCode($request->get('CIUD_UBI'))->CIUDAD;
-        $request['EDAD_INDP'] = $antig_ind->diffInYears(Carbon::now());
-        $request['ANTIG']     = $antig->diffInMonths(Carbon::now());
-        $request['EDAD']      = $birthday->diffInYears(Carbon::now());
-        $request['SUC']       = $request->get('CIUD_UBI');
-        $request['CIUD_UBI']  = $subsidiaryCityName;
-        $request['POSEEVEH']  = "S";
+        $authAssessor = (Auth::guard('assessor')->check()) ? Auth::guard('assessor')->user()->CODIGO : NULL;
+        if (Auth::user()) {
+            $authAssessor = (Auth::user()->codeOportudata != NULL) ? Auth::user()->codeOportudata : $authAssessor;
+        }
+        $identificationNumber = trim($request->get('CEDULA'));
+        $customer             = $this->customerInterface->checkIfExists($identificationNumber);
+        $clienteWeb           = 1;
+        $usuarioActualizacion = "";
+        $assessorCode = ($authAssessor !== NULL) ? $authAssessor : 998877;
+
+        $clienteWeb = ($customer->CLIENTE_WEB != '') ? $customer->CLIENTE_WEB : 1 ;
+        $usuarioCreacion = ($customer->USUARIO_CREACION != '') ? $customer->USUARIO_CREACION : (string) $assessorCode ;
+        $usuarioActualizacion = (string) $assessorCode;
+
+        $cityExp            = $this->cityInterface->getCityByCode($request->input('CIUD_EXP'));
+        $antig_ind          = new Carbon($request['FEC_CONST']);
+        $birthday           = new Carbon($request->FEC_NAC);
+        $subsidiaryCityName = $this->subsidiaryInterface->getSubsidiaryCityByCode($request->get('CIUD_UBI'))->CIUDAD;
+        $cityUbi            = $this->cityInterface->getCityByName($subsidiaryCityName);
+        $antig              = new Carbon($request['FEC_ING']);
+
+        $request['CIUD_EXP']              = $cityExp->NOMBRE;
+        $request['ID_CIUD_EXP']           = $cityExp->ID_DIAN;
+        $request['FEC_ING']               = $request->input('FEC_ING')."-01";
+        $request['FEC_CONST']             = $request->input('FEC_CONST')."-01";
+        $request['EDAD_INDP']             = $antig_ind->diffInYears(Carbon::now());
+        $request['ANTIG']                 = $antig->diffInMonths(Carbon::now());
+        $request['EDAD']                  = $birthday->diffInYears(Carbon::now());
+        $request['SUC']                   = $request->get('CIUD_UBI');
+        $request['TIPOCLIENTE']           = 'OPORTUYA';
+        $request['SUBTIPO']               = 'WEB';
+        $request['CLIENTE_WEB']           = $clienteWeb;
+        $request['USUARIO_CREACION']      = $usuarioCreacion;
+        $request['USUARIO_ACTUALIZACION'] = $usuarioActualizacion;
+        $request['TRAT_DATOS']            = "SI";
+        $request['FECHA_ACTUALIZACION']   = date('Y-m-d H                 :i:s');
+        $request['DEPTO']                 = $cityUbi->DEPARTAMENTO;
+        $request['ID_CIUD_UBI']           = $cityUbi->ID_DIAN;
+        $request['CIUD_UBI']              = $subsidiaryCityName;
+        $request['POSEEVEH']              = "S";
 
         foreach ($request->input() as $key => $value) {
             $request[$key] = trim(strtoupper($request[$key]));
         }
-        
+
         $this->customerInterface->updateOrCreateCustomer($request->input());
 
         if (empty($this->customerCellPhoneInterface->checkIfExists($request->input('CEDULA'), $request->input('CELULAR')))) {
@@ -62,5 +94,22 @@ class SegurosController extends Controller
             $clienteCelular['FECHA'] = date("Y-m-d H:i:s");
             $this->customerCellPhoneInterface->createCustomerCellPhone($clienteCelular);
         }
+    }
+
+    public function getInfoForm()
+    {
+        // Ciudad de ubicación
+        $query = "SELECT CODIGO as value, CIUDAD as label FROM SUCURSALES WHERE PRINCIPAL = 1 ORDER BY CIUDAD ASC";
+        $resp = DB::connection('oportudata')->select($query);
+
+        // Ciudad de nacimiento
+        $query2 = "SELECT `CODIGO` as value, `NOMBRE` as label FROM `CIUDADES` WHERE `STATE` = 'A' ORDER BY NOMBRE ";
+        $resp2 = DB::connection('oportudata')->select($query2);
+
+        // Banco Pensionado
+        $query3 = "SELECT `CODIGO` as value, `BANCO` as label FROM BANCO ";
+        $resp3 = DB::connection('oportudata')->select($query3);
+
+        return response()->json(['ubicationsCities' => $resp, 'cities' => $resp2, 'banks' => $resp3]);
     }
 }
