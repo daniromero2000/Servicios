8244e105931fb7812d8a63fafb7d5f0f87d6f09b
Daniel Alejandro Romero Morales <daniel.romero@laso.local>
2020-01-24 19:59:52 -0500

    24 January Cartera

---
+++ resources/views/digitalchannelleads/layouts/card_total_quotes.blade.php
@@ -0,0 +1,15 @@
+<div class="small-box bg-success">
+    <div class="inner">
+        <h4 class="mt-2">${{ number_format ($pricesTotal) }}</h3>
+            <p style="margin-bottom: -4px !important;">Total Cotizado </p>
+    </div>
+    <br>
+    <div class="icon">
+        <i class="fas fa-shopping-cart"></i>
+    </div>
+    <div style="margin-top: -5px;" class="text-right mr-2">
+        <span class="info-box-text text-right">
+            <a href="/Administrator/digitalchannelleads" style="color: white; !important">Ver
+                Más</a></span>
+    </div>
+</div>
