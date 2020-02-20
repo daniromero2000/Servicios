5221cdae20214af0c219592feabfaf822b60794b
Daniel Alejandro Romero Morales <daniel.romero@laso.local>
2020-02-20 11:11:20 -0500

    botones de consultas

--- resources/views/customers/show.blade.php
+++ resources/views/customers/show.blade.php
@@ -113,5 +113,25 @@
                 <a href="{{ URL::previous() }}" class="btn btn-primary ml-auto mr-3 mb-2 ">Regresar</a>
             </div>
         </div>
+
+        <div class="modal modalSteps fade hide" data-backdrop="static" data-keyboard="false" id="proccessConsult"
+            tabindex="-1" role="dialog" aria-hidden="true">
+            <div class="modal-dialog modalPrincipal" role="document">
+                <div class="modal-content">
+                    <div class="modal-body">
+                        <div class="text-center" style="padding: 50px;">
+                            <img src="{{ asset('images/gif-load.gif') }}" alt="">
+                            <p class="text-procces">
+                                Procesando Solicitud...<br>
+                                <span style="font-size: 15px; font-style:italic; font-weight:normal">*No te desesperes,
+                                    se
+                                    están realizando las consultas necesarias, esto
+                                    puede tomar un tiempo de aproximadamente 2 minutos</span>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
 </section>
 @endsection
