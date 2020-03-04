e7066d093ee2207aa3f746ab4a40fabff40bfe62
Daniel Alejandro Romero Morales <daniel.romero@laso.local>
2020-03-04 17:58:13 -0500

    asserssorController

--- resources/views/digitalchannelleads/layouts/pie_statuses/pie_statuses_Ecommerces.blade.php
+++ resources/views/digitalchannelleads/layouts/pie_statuses/pie_statuses_Ecommerces.blade.php
@@ -1,7 +1,7 @@
 <div class=" col-md-8 col-lg-12">
     <div class="card">
         <div class="card-header">
-            <h3 class="card-title"> Resumen Estados Leads E-commerce </h3>
+            <h3 class="card-title"> Resumen Estados Leads Ecommerce </h3>
             <div class="card-tools">
                 <button type="button" class="btn btn-tool" data-card-widget="collapse"><i class="fas fa-minus"></i>
                 </button>
