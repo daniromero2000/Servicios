fa430d33feb0a414bd9d9766d7de5d86d6a6a828
Daniel Alejandro Romero Morales <daniel.romero@laso.local>
2020-03-12 08:37:01 -0500

    Campanas leads

--- resources/views/layouts/admin/tables/table_digital_channel_leads_status_generals.blade.php
+++ resources/views/layouts/admin/tables/table_digital_channel_leads_status_generals.blade.php
@@ -12,27 +12,17 @@ use Carbon\Carbon;
     </thead>
     <tbody class="body-table">
       @foreach($datas as $data)
-
+      @php
+      $date = $data->leadStatusesLogs->last();
+      @endphp
       <tr>
-        @php
-        $date = $data->leadStatusesLogs->last();
-        @endphp
-        <td>
-          @if ($date)
-          @if ($date->created_at->diffInDays(Carbon::now()) <= 1) <span class="text-center badge"
-            style="color: white ; background-color: #0bb010" class=" btn btn-info btn-block"><i
-              class="fa fa-bell-o"></i> </span>
-            @endif
-            @if ($date->created_at->diffInDays(Carbon::now()) <= 2 && $date->created_at->diffInDays(Carbon::now()) >1)
-              <span class="text-center badge" style="color: white ; background-color: #ff9900"
-                class=" btn btn-info btn-block"><i class="fa fa-bell-o"></i> </span>
-              @endif
-              @if ($date->created_at->diffInDays(Carbon::now()) >= 3)
-              <span class="text-center badge" style="color: white ; background-color: #b0130b"
-                class=" btn btn-info btn-block"><i class="fa fa-bell-o"></i> </span>
-              @endif
-              @endif
-        </td>
+        <td><span class="text-center badge"
+            style="color: white ; background-color:
+                    @if($date->created_at->diffInDays(Carbon::now()) <= 1) #0bb010 @endif
+                    @if($date->created_at->diffInDays(Carbon::now()) <= 2 && $date->created_at->diffInDays(Carbon::now()) >1) #ff9900 @endif
+                    @if($date->created_at->diffInDays(Carbon::now()) >= 3) #b0130b @endif
+                    @if($data->state === 2 || $data->state === 5 || $data->state === 6 || $data->state === 9 || $data->state === 4 || $data->state === 7) gray @endif"
+            class=" btn btn-info btn-block"><i class="fa fa-bell-o"></i> </span></td>
         <td> @if($data->leadStatuses) <span class="text-center badge"
             style="color: white ; background-color: {{$data->leadStatuses->color }}"
             class="btn btn-info btn-block">{{ $data->leadStatuses->status}}</span> @endif</td>
