a2ee4da8d16eb41a5d367bc4f3d9a9e35ecca0a1
Daniel Alejandro Romero Morales <daniel.romero@laso.local>
2020-03-19 11:37:20 -0500

    Campanas leads

--- resources/views/factoryrequests/dashboard.blade.php
+++ resources/views/factoryrequests/dashboard.blade.php
@@ -16,16 +16,152 @@
 </div>
 <div class="container-fluid">
   <div class="row mt-2">
-    <div class="col-sm-12 col-md-5 col-lg-4">
+    <div class="order-md-last  col-md-7 col-lg-8">
+      <!-- debe ir oculta -->
+      <div hidden class="card card-danger">
+        <div class="card-header">
+          <h3 class="card-title">Donut Chart</h3>
+
+          <div class="card-tools">
+            <button type="button" class="btn btn-tool" data-card-widget="collapse"><i class="fas fa-minus"></i>
+            </button>
+            <button type="button" class="btn btn-tool" data-card-widget="remove"><i class="fas fa-times"></i></button>
+          </div>
+        </div>
+        <div class="card-body">
+          <canvas id="donutChart" style="height:230px; min-height:230px"></canvas>
+        </div>
+      </div>
+      <!-- TORTA -->
+      <div class="card">
+        <div class="card-body">
+          <div class="col-12">
+            @include('layouts.admin.date_filter', ['route' => route('factory_dashboard')])
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-6 col-md-6 col-xl-3">
+          <!-- /.info-box -->
+          <div class="small-box bg-success">
+            <div class="inner">
+              <h2 class="titleCardNumber titleCardNumberForTotals">@if ($valuesOfStatusesAprobados !=0 )
+                ${{number_format( $valuesOfStatusesAprobados )}} @else $0
+                @endif </h2>
+              @if ($_GET && $_GET['from'] != '' && $_GET['to'] != '')
+              <p class="textCardNumber textCardNumberForTotals">Total Vendidos</p>
+              @else
+              <p class="textCardNumber textCardNumberForTotals">Total Vendidos en este mes</p>
+              @endif
+            </div>
+            <div class="icon">
+              <i class="ion ion-stats-bars"></i>
+            </div>
+          </div>
+        </div>
+        <div class="col-6 col-md-6 col-xl-3">
+          <!-- /.info-box -->
+          <div class="small-box bg-danger">
+            <div class="inner">
+              <h2 class="titleCardNumber titleCardNumberForTotals">@if ($valuesOfStatusesNegados !=0 )
+                ${{number_format( $valuesOfStatusesNegados )}}@else $0
+                @endif </h2>
+              @if ($_GET && $_GET['from'] != '' && $_GET['to'] != '')
+              <p class="textCardNumber textCardNumberForTotals">Total Negados</p>
+              @else
+              <p class="textCardNumber textCardNumberForTotals">Total Negados en este mes</p>
+              @endif
+            </div>
+            <div class="icon">
+              <i class="ion ion-stats-bars"></i>
+            </div>
+          </div>
+        </div>
+        <div class="col-6 col-md-6 col-xl-3">
+          <!-- /.info-box -->
+          <div class="small-box bg-info">
+            <div class="inner">
+              <h2 class="titleCardNumber titleCardNumberForTotals">@if ($valuesOfStatusesDesistidos !=0 )
+                ${{number_format( $valuesOfStatusesDesistidos )}} @else $0 @endif </h2>
+              @if ($_GET && $_GET['from'] != '' && $_GET['to'] != '')
+              <p class="textCardNumber textCardNumberForTotals">Total Desistidos</p>
+              @else
+              <p class="textCardNumber textCardNumberForTotals">Total Desistidos en este mes</p>
+              @endif
+            </div>
+            <div class="icon">
+              <i class="ion ion-stats-bars"></i>
+            </div>
+          </div>
+        </div>
+        <div class="col-6 col-md-6 col-xl-3">
+          <!-- /.info-box -->
+          <div class="small-box bg-secondary">
+            <div class="inner">
+              <h2 class="titleCardNumber titleCardNumberForTotals">@if ($valuesOfStatusesPendientes !=0 )
+                ${{number_format( $valuesOfStatusesPendientes )}} @else $0 @endif</h2>
+              @if ($_GET && $_GET['from'] != '' && $_GET['to'] != '')
+              <p class="textCardNumber textCardNumberForTotals">Total Pendientes</p>
+              @else
+              <p class="textCardNumber textCardNumberForTotals">Total Pendientes en este mes</p>
+              @endif
+            </div>
+            <div class="icon">
+              <i class="ion ion-stats-bars"></i>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="card">
+        <div class="card-header">
+          <h3 class="card-title">Estados Solicitudes</h3>
+          <div class="card-tools">
+            <button type="button" class="btn btn-tool" data-card-widget="collapse"><i class="fas fa-minus"></i>
+            </button>
+            <button type="button" class="btn btn-tool" data-card-widget="remove"><i class="fas fa-times"></i></button>
+          </div>
+        </div>
+        <div class="card-body">
+          <div class="row">
+            <div class="col-12">
+              <canvas id="pieChart" style="height:334px; min-height:300px"></canvas>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- PORCENTAJES -->
+      <div class="card ">
+        <div class="card-header">
+          <h3 class="card-title">Estados Solicitudes</h3>
+          <div class="card-tools">
+            <button type="button" class="btn btn-tool" data-card-widget="collapse"><i class="fas fa-minus"></i>
+            </button>
+            <button type="button" class="btn btn-tool" data-card-widget="remove"><i class="fas fa-times"></i></button>
+          </div>
+        </div>
+        <div class="card-body row justify-content-center">
+          <div class="col-10">
+            <div class="chart">
+              <canvas id="barChart" style="height:230px; min-height:230px"></canvas>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="order-md-first col-sm-12 col-md-5 col-lg-4">
       <div class="col-12 col-sm-12">
         <div class="row">
           <div class="col-12">
             <div class="small-box bg-primary">
               <div class="inner">
-                <h2>{{ $totalStatuses }}</h2>
-                <p style="margin-bottom: -4px !important;">Solicitudes Crédito en este mes</p>
+                <h2 class="titleCardNumber ">{{ $totalStatuses }}</h2>
+                @if ($_GET && $_GET['from'] != '')
+                <p class="textCardNumber textCardNumberForTotals">Total Solicitudes</p>
+                @else
+                <p class="textCardNumber textCardNumberForTotals">Solicitudes Crédito en este mes</p>
+                @endif
               </div>
-              <div class="icon mt-3">
+              <div class="icon">
                 <i class="ion ion-stats-bars"></i>
               </div>
               <div class="text-right mr-2">
@@ -38,15 +174,18 @@
           <div class="col-12 ">
             <div class="small-box bg-success">
               <div class="inner">
-                <h4 class="mt-2">${{ number_format ($factoryRequestsTotal) }}
+                <h4 class="titleCardNumber titleCardNumberForTotals">${{ number_format ($factoryRequestsTotal) }}
                 </h4>
-                <p style="margin-bottom: -4px !important;">Total</p>
+                @if ($_GET && $_GET['from'] != '')
+                <p class="textCardNumber textCardNumberForTotals">Total </p>
+                @else
+                <p class="textCardNumber textCardNumberForTotals">Total en este mes</p>
+                @endif
               </div>
-              <br>
               <div class="icon">
                 <i class="fas fa-shopping-cart"></i>
               </div>
-              <div style="margin-top: -5px;" class="text-right mr-2">
+              <div class="text-right mr-2">
                 <span class="info-box-text text-right">
                   <a href="/Administrator/factoryrequests" style="color: white; !important">Ver
                     Más</a></span>
@@ -193,67 +332,7 @@
           </div>
         </div>
         <div class="card-body">
-          <canvas id="pieChart2" style="height:200px; min-height:auto"></canvas>
-        </div>
-      </div>
-    </div>
-    <div class=" col-md-7 col-lg-8">
-      <!-- debe ir oculta -->
-      <div hidden class="card card-danger">
-        <div class="card-header">
-          <h3 class="card-title">Donut Chart</h3>
-
-          <div class="card-tools">
-            <button type="button" class="btn btn-tool" data-card-widget="collapse"><i class="fas fa-minus"></i>
-            </button>
-            <button type="button" class="btn btn-tool" data-card-widget="remove"><i class="fas fa-times"></i></button>
-          </div>
-        </div>
-        <div class="card-body">
-          <canvas id="donutChart" style="height:230px; min-height:230px"></canvas>
-        </div>
-      </div>
-      <!-- TORTA -->
-      <div class="card">
-        <div class="card-body">
-          <div class="col-12">
-            @include('layouts.admin.date_filter', ['route' => route('factory_dashboard')])
-          </div>
-        </div>
-      </div>
-      <div class="card">
-        <div class="card-header">
-          <h3 class="card-title">Estados Solicitudes</h3>
-          <div class="card-tools">
-            <button type="button" class="btn btn-tool" data-card-widget="collapse"><i class="fas fa-minus"></i>
-            </button>
-            <button type="button" class="btn btn-tool" data-card-widget="remove"><i class="fas fa-times"></i></button>
-          </div>
-        </div>
-        <div class="card-body">
-          <div class="row">
-            <div class="col-12">
-              <canvas id="pieChart" style="height:334px; min-height:300px"></canvas>
-            </div>
-          </div>
-        </div>
-      </div>
-      <!-- PORCENTAJES -->
-      <div class="card ">
-        <div class="card-header">
-          <h3 class="card-title">Estados Solicitudes</h3>
-          <div class="card-tools">
-            <button type="button" class="btn btn-tool" data-card-widget="collapse"><i class="fas fa-minus"></i>
-            </button>
-            <button type="button" class="btn btn-tool" data-card-widget="remove"><i class="fas fa-times"></i></button>
-          </div>
-        </div>
-        <div class="card-body row justify-content-center">
-          <div class="col-10">
-            <div class="chart">
-              <canvas id="barChart" style="height:230px; min-height:230px"></canvas>
-            </div>
-          </div>
+          <canvas id="pieChart2" style="height:302px; min-height:auto"></canvas>
         </div>
       </div>
     </div>
