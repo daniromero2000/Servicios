563fa62022be160f41180ecfe61db088bae92620
Juan Sebastian Ormaza <desarrollo@lagobo.com>
2020-04-20 08:52:59 -0500

    Migraciones

---
+++ database/migrations/2020_04_18_111529_create_factors_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFactorsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('factors', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('creation_user_id')->unsigned();
+            $table->string('name', 40);
+            $table->float('value', 4,2);
+            $table->tinyInteger('checked', 1)->default(0)->comment('0: No comprobada, 1: Comprobada');
+            $table->integer('checked_user_id')->unsigned()->nullable();
+            $table->date('start_date');
+            $table->date('end_date');
+            $table->timestamps();
+            $table->softDeletes();
+            $table->foreign('creation_user_id')->references('id')->on('users');
+            $table->foreign('checked_user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('factors');
+    }
+}
