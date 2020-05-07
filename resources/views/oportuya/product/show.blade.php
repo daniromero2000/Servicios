11fb1e1a6aaefe72bf2e8cd21ff2ba57a36aeb94
Daniel Alejandro Romero Morales <daniel.romero@laso.local>
2020-05-07 15:27:23 -0500

    6/05/2020

--- resources/views/oportuya/product/show.blade.php
+++ resources/views/oportuya/product/show.blade.php
@@ -178,7 +178,7 @@ array_push($imagenes, [$productImages[$key], $key]);
                             </p>
                             <p class="card-text text-dues-deal-product">* Cuota semanal</p>
                             <a href="/step1" class="btn card-products-button btn-primary"
-                                style="margin-left: 11px;">Solicitar
+                                style="margin-left: 15px;">Solicitar
                                 aqui</a>
                         </div>
 
