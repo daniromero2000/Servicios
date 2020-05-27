cc1998247bc2a4adc619d0f78c4fe3ce25c3a948
Daniel Alejandro Romero Morales <daniel.romero@laso.local>
2020-05-27 09:52:10 -0500

    26/05/2020

--- app/Http/Controllers/Front/ConfrontaCustomers/ConfrontaCustomerController.php
+++ app/Http/Controllers/Front/ConfrontaCustomers/ConfrontaCustomerController.php
@@ -4,16 +4,12 @@ namespace App\Http\Controllers\Front\ConfrontaCustomers;
 
 use App\cliCel;
 use App\Entities\Cities\Repositories\Interfaces\CityRepositoryInterface;
-use App\Imagenes;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Entities\Subsidiaries\Repositories\Interfaces\SubsidiaryRepositoryInterface;
 use App\Entities\Customers\Repositories\Interfaces\CustomerRepositoryInterface;
 use App\Entities\CustomerCellPhones\Repositories\Interfaces\CustomerCellPhoneRepositoryInterface;
 use App\Entities\ConfrontForms\Repositories\Interfaces\ConfrontFormRepositoryInterface;
-use App\Entities\Customers\Customer;
-use Carbon\Carbon;
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 
 class ConfrontaCustomerController extends Controller
@@ -30,7 +26,7 @@ class ConfrontaCustomerController extends Controller
         $this->subsidiaryInterface        = $subsidiaryRepositoryInterface;
         $this->customerInterface          = $customerRepositoryInterface;
         $this->customerCellPhoneInterface = $customerCellPhoneRepositoryInterface;
-        $this->confrontFormInterface         = $confrontFormRepositoryInterface;
+        $this->confrontFormInterface      = $confrontFormRepositoryInterface;
         $this->cityInterface              = $cityRepositoryInterface;
     }
 
@@ -99,20 +95,14 @@ class ConfrontaCustomerController extends Controller
     public function update(Request $request, $id)
     {
 
-
         $datas = [];
         foreach ($request->customerData as $key => $value) {
             $datas[$request->customerData[$key]['name']] = $request->customerData[$key]['value'];
         }
         $queryExistCel = DB::connection('oportudata')->select("SELECT COUNT(*) as total FROM `CLI_CEL` WHERE `IDENTI` = :cedula AND `NUM` = :telefono ", ['cedula' => $datas['CEDULA'], 'telefono' => $datas['CELULAR']]);
         if ($queryExistCel[0]->total == 0) {
-            $clienteCelular          = new cliCel;
-            $clienteCelular->IDENTI  = $datas['CEDULA'];
-            $clienteCelular->NUM     = $datas['CELULAR'];
-            $clienteCelular->TIPO    = 'CEL';
-            $clienteCelular->CEL_VAL = 0;
-            $clienteCelular->FECHA   = date("Y-m-d H:i:s");
-            $clienteCelular->save();
+            $dataCliCel = ['IDENTI' => $datas['CEDULA'], 'NUM' => $datas['CELULAR'], 'TIPO' => 'CEL', 'CEL_VAL' => '0', 'FECHA' => date("Y-m-d H:i:s")];
+            $customer = cliCel::create($dataCliCel);
         }
         $customer = $this->customerInterface->updateOrCreateCustomer($datas);
 
