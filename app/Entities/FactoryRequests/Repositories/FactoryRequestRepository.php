97b25d9c6e7f7539dafb5e72bb706e558ba5b4f6
Daniel Alejandro Romero Morales <daniel.romero@laso.local>
2020-05-30 15:55:45 -0500

    29/05/2020

--- app/Entities/FactoryRequests/Repositories/FactoryRequestRepository.php
+++ app/Entities/FactoryRequests/Repositories/FactoryRequestRepository.php
@@ -2270,12 +2270,11 @@ class FactoryRequestRepository implements FactoryRequestRepositoryInterface
             dd($e);
         }
     }
-
     public function countFactoryRequestsStatusesTurn($from, $to)
     {
         try {
             return  $this->model->select('ESTADO', DB::raw('count(*) as total'))
-            ->where('state', 'A')
+                ->where('state', 'A')
                 ->whereBetween('FECHASOL', [$from, $to])
                 ->where('ESTADO', '!=', 'EN SUCURSAL')
                 ->groupBy('ESTADO')
