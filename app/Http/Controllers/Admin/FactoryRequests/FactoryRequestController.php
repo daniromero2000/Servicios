22566ac7dfb895efa528e31bfe1dc384441929c2
Alejandro Castañeda <desarrollo1@lagobo.com>
2020-06-18 23:34:21 -0500

    statuses release

--- app/Http/Controllers/Admin/FactoryRequests/FactoryRequestController.php
+++ app/Http/Controllers/Admin/FactoryRequests/FactoryRequestController.php
@@ -156,7 +156,8 @@ class FactoryRequestController extends Controller
                 $secondsDays = $date1->diffInSeconds($date2) / 28800;
                 if ($datas[$key]->oportudataUser != "" && ($datas[$key]->oportudataUser->PERFIL  == 1 || $datas[$key]->oportudataUser->PERFIL  == 2)) {
                     if ($secondsDays > 1) {
-                        $removeSeconds = $secondsDays * 57600;
+                        $secondsDays = $secondsDays - 1;
+                        $removeSeconds = $secondsDays * 28800;
                         $data['fabrica'] += $date1->diffInSeconds($date2);
                         $data['fabrica'] = $data['fabrica'] - $removeSeconds;
                     } else {
@@ -164,7 +165,8 @@ class FactoryRequestController extends Controller
                     }
                 } else {
                     if ($secondsDays > 1) {
-                        $removeSeconds = $secondsDays * 57600;
+                        $secondsDays = $secondsDays - 1;
+                        $removeSeconds = $secondsDays * 28800;
                         $data['sucursal'] += $date1->diffInSeconds($date2);
                         $data['sucursal'] = $data['sucursal'] - $removeSeconds;
                     } else {
