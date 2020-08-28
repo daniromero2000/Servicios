7c2c6d78bae94a69bb5f84ff863b87e5442f6fcf
Daniel Alejandro Romero Morales <daniel.romero@laso.local>
2020-08-28 10:49:16 -0500

    Customers

--- app/Entities/PaymentTimes/PaymentTime.php
+++ app/Entities/PaymentTimes/PaymentTime.php
@@ -9,11 +9,16 @@ class PaymentTime extends Model
     protected $table = 'payment_time';
 
     protected $fillable = [
-               
+        'identificationNumber',
+        'credit',
+        'period',
+        'payment_fee',
+        'payment_date',
+        'expired_time'
     ];
 
     public function customer()
     {
         return $this->belongsTo(Customer::class);
-    } 
+    }
 }
