c056ba781f3f24d81a0a42e398db2d9e027c74cd
Daniel Alejandro Romero Morales <daniel.romero@laso.local>
2020-09-07 18:07:35 -0500

    Leads

--- resources/views/seguros/index.blade.php
+++ resources/views/seguros/index.blade.php
@@ -67,7 +67,8 @@
 						{{ csrf_field() }}
 						<input class="form-control input-form-third-sector" id="exampleInputEmail1" name="channel"
 							value="1" aria-describedby="emailHelp" hidden>
-
+						<input class="form-control input-form-third-sector" id="exampleInputEmail1" name="assessor_id"
+							value="262" aria-describedby="emailHelp" hidden>
 						<input class="form-control input-form-third-sector" id="exampleInputEmail1" name="lead_area_id"
 							value="1" aria-describedby="emailHelp" hidden>
 						<input class="form-control input-form-third-sector" id="exampleInputEmail1" name="typeService"
