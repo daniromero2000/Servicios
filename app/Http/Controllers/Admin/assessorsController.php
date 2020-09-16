71d6f7d8c52112f8b47a24c04fe455ea38e0690c
Alejandro Castañeda <desarrollo1@lagobo.com>
2020-09-16 16:54:52 -0500

    policy refactoring

--- app/Http/Controllers/Admin/assessorsController.php
+++ app/Http/Controllers/Admin/assessorsController.php
@@ -584,6 +584,7 @@ class assessorsController extends Controller
 	private function validatePolicyCredit_new($customer)
 	{
 		$this->daysToIncrement = $this->consultationValidityInterface->getConsultationValidity()->pub_vigencia;
+		$customer         = $this->customerInterface->findCustomerById($customer->CEDULA);
 		$lastIntention         = $this->intentionInterface->validateDateIntention($customer->CEDULA,  $this->daysToIncrement);
 		$assessorCode          = $this->userInterface->getAssessorCode();
 
