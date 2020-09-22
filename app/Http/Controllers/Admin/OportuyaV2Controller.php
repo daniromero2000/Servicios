8733b5fb36ab8e7c5902a66787656a91850156b5
Alejandro Castañeda <desarrollo1@lagobo.com>
2020-09-22 13:58:13 -0500

    Cards definitions refactoring

--- app/Http/Controllers/Admin/OportuyaV2Controller.php
+++ app/Http/Controllers/Admin/OportuyaV2Controller.php
@@ -1444,8 +1444,7 @@ class OportuyaV2Controller extends Controller
 		$customerFactoryRequest = $this->factoryRequestInterface->addFactoryRequest($requestData);
 		$this->codebtorInterface->createCodebtor($customerFactoryRequest->SOLICITUD);
 		$this->secondCodebtorInterface->createSecondCodebtor($customerFactoryRequest->SOLICITUD);
-		$factoryRequest = $this->factoryRequestInterface->findFactoryRequestById($customerFactoryRequest->SOLICITUD);
-		$factoryRequest->states()->attach($estado, ['usuario' => $customer->USUARIO_ACTUALIZACIONe]);
+		$customerFactoryRequest->states()->attach($estado, ['usuario' => $customer->USUARIO_ACTUALIZACIONe]);
 		return $customerFactoryRequest;
 	}
 
