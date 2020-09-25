8f1b674bf1c5e3f30294bca7b80de3f384a7d9a6
Alejandro Castañeda <desarrollo1@lagobo.com>
2020-09-25 12:41:32 -0500

    policy refactoring

--- app/Http/Controllers/Admin/FactoryRequests/FactoryRequestController.php
+++ app/Http/Controllers/Admin/FactoryRequests/FactoryRequestController.php
@@ -204,7 +204,7 @@ class FactoryRequestController extends Controller
     {
         $factoryRequest = $this->factoryRequestInterface->findFactoryRequestById($solicitud);
         $factoryRequest->ASESOR_DIG = auth()->user()->id;
-        $factoryRequest->CODASESOR = auth()->user()->id;
+        $factoryRequest->CODASESOR = auth()->user()->email;
         return response()->json($factoryRequest->save());
     }
 
