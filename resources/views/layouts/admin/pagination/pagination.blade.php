5bb5ed46dffbbd28568b39e3445799e9ca369df3
Daniel Alejandro Romero Morales <daniel.romero@laso.local>
2020-09-29 12:17:19 -0500

    Cotizaciones

--- resources/views/layouts/admin/pagination/pagination.blade.php
+++ resources/views/layouts/admin/pagination/pagination.blade.php
@@ -1,13 +1,10 @@
 <nav aria-label="...">
     <ul class="pagination d-flex justify-content-center">
         <li class="page-item">
-            <a id="previous" name="previous" type="submit" class="page-link btn-sm-reset"
-                href="{{ route("$optionsRoutes.index", ['skip' => ($skip - 1)] ) }}" @if ($skip<1 ) hidden
-                @endif>Anterior</a>
+            <a id="previous" name="previous" type="submit" class="page-link btn-sm-reset" href="{{ route("$optionsRoutes.index", ['skip' => ($skip - 1) , 'q' => request()->input('q')] ) }}" @if ($skip<1 ) hidden @endif>Anterior</a>
         </li>
         <li class="page-item">
-            <a type="submit" type="submit" class="page-link btn-sm-reset"
-                href="{{ route("$optionsRoutes.index", ['skip' => ($skip + 1)] ) }}">Siguiente</a>
+            <a type="submit" type="submit" class="page-link btn-sm-reset" href="{{ route("$optionsRoutes.index", ['skip' => ($skip + 1), 'q' => request()->input('q')] ) }}">Siguiente</a>
         </li>
     </ul>
 </nav>
