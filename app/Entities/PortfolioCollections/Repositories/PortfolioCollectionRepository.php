2ad54ddd96cd020dd3c639a1942af02cedb606b8
Daniel Alejandro Romero Morales <daniel.romero@laso.local>
2020-12-01 13:12:28 -0500

    portfolio_collections

--- app/Entities/PortfolioCollections/Repositories/PortfolioCollectionRepository.php
+++ app/Entities/PortfolioCollections/Repositories/PortfolioCollectionRepository.php
@@ -81,7 +81,7 @@ class PortfolioCollectionRepository implements PortfolioCollectionRepositoryInte
             CURLOPT_POSTFIELDS => "{ 'ProjectId': '1018', 'ApplicationId': '13', 'ButtonId': '46820', 'Table': 'false', 'Controls': [ { 'Id': 'numeroTransaccion', 'Valor': '201912160278133129' }, { 'Id': 'nombreEmpresa', 'Valor': 'CREDICORP CAPITAL COLOMBIA S.A.' }, { 'Id': 'nitEmpresa', 'Valor': '8600681825' }, { 'Id': 'referenciaPago', 'Valor': '123234234' }, { 'Id': 'nombreBanco', 'Valor': 'Bancolombia' }, { 'Id': 'tipoEntidad', 'Valor': 'ENTIDAD BANCARIA' }, { 'Id': 'codigoBanco', 'Valor': '19' }, { 'Id': 'oficina', 'Valor': 'PLAZA DE LAS AMERICAS' }, { 'Id': 'ciudad', 'Valor': 'BOGOTA' }, { 'Id': 'codigoCuenta', 'Valor': '24651' }, { 'Id': 'fechaPago', 'Valor': '10/09/2020' }, { 'Id': 'valorPago', 'Valor': '500000' }, { 'Id': 'origen', 'Valor': '4' } ] }",
             CURLOPT_HTTPHEADER => array(
                 "accept: application/json",
-                "authorization: Bearer YjDviCv971qv5_sRSOSOZSdLgIluGFnmOAj3ouSclzYXOqGtEzVEi9P1j_KPSFxRRf2YeavT_80RiYAT_-70_UkTV0qlNOQ71DjJqU7hklO8shwPxiF62vCVfc-qUG-KQKm5sDVJ55ez3dvh5Yl5-IY6zyoNiwJgrtgkvdGszUO5ZSc8rj4-zZViVF9PdzwG81HaYWLKwZW14JUe33mlV_fdvaA-gK7Ps6f76B9YNcZH6c2EIa_9BGZ9g6QLrPaPlNi4sZ8WSeXrLmSud82cS5uUumYrkVMXtVDk4ynVTZ4WTnzlbSQOi_WA5Y4cqNeysBbprwPhwM0RS3jzcHx7ww ",
+                "authorization: Bearer ". $data['token'],
                 "content-type: application/json"
             ),
         ));
