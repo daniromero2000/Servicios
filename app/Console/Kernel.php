8141b58839e0f3a3526bdb9c55ca24d43970fb27
Daniel Alejandro Romero Morales <daniel.romero@laso.local>
2021-02-18 15:57:05 -0500

    Correos

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,8 +25,8 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->command('sitemap:generate')->weekly();
-        $schedule->command('verifyInvoiceExpiration')->dailyAt('16:56');
-        $schedule->command('enableInvoicesForPayment')->dailyAt('16:56');
+        $schedule->command('verifyInvoiceExpiration')->dailyAt('16:00');
+        $schedule->command('enableInvoicesForPayment')->dailyAt('16:00');
     }
 
     /**
